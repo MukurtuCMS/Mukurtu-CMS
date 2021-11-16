ea53ff5938ae252a0304fc6c8ff89eb81bf4674c
Steve Taylor <steve_taylor@wsu.edu>
2021-11-16 09:22:27 -0800

    Collections computed field

---
+++ modules/mukurtu_collection/src/Plugin/Field/MukurtuInCollectionFieldItemsList.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Drupal\mukurtu_collection\Plugin\Field;
+
+use Drupal\Core\Field\EntityReferenceFieldItemList;
+use Drupal\Core\TypedData\ComputedItemListTrait;
+
+class MukurtuInCollectionFieldItemsList extends EntityReferenceFieldItemList {
+
+  use ComputedItemListTrait;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function computeValue() {
+    $this->ensurePopulated();
+  }
+
+  protected function ensurePopulated() {
+    $entity = $this->getEntity();
+
+    $query = \Drupal::entityQuery('node')
+      ->condition('type', 'collection')
+      ->condition(MUKURTU_COLLECTION_FIELD_NAME_ITEMS, $entity->id())
+      ->condition(MUKURTU_COLLECTION_FIELD_NAME_COLLECTION_TYPE, 'default')
+      ->condition('status', TRUE);
+    $results = $query->execute();
+
+    if (!empty($results)) {
+      $delta = 0;
+      foreach ($results as $result) {
+        $this->list[$delta] = $this->createItem($delta, $result);
+        $delta++;
+      }
+    }
+  }
+
+}
