d13b735d8e5e1a56048369dd57f5617818652784
Steve Taylor <steve_taylor@wsu.edu>
2022-03-04 09:54:33 -0800

    First attempt at protocol entity access control

--- modules/mukurtu_protocol/src/ProtocolAccessControlHandler.php
+++ modules/mukurtu_protocol/src/ProtocolAccessControlHandler.php
@@ -6,6 +6,7 @@ use Drupal\Core\Entity\EntityAccessControlHandler;
 use Drupal\Core\Entity\EntityInterface;
 use Drupal\Core\Session\AccountInterface;
 use Drupal\Core\Access\AccessResult;
+use Drupal\og\Og;
 
 /**
  * Access controller for the Protocol entity.
@@ -19,13 +20,48 @@ class ProtocolAccessControlHandler extends EntityAccessControlHandler {
    */
   protected function checkAccess(EntityInterface $entity, $operation, AccountInterface $account) {
     /** @var \Drupal\mukurtu_protocol\Entity\ProtocolInterface $entity */
+    $communities = $entity->getCommunities();
 
-    $ogAccessService = \Drupal::service('og.access');
-    // @todo Return after implementing community.
-    return AccessResult::allowed();
-   // $community = $entity->getCommunity();
-    if ($community) {
-      return $ogAccessService->userAccessGroupContentEntityOperation($operation, $community, $entity, $account);
+    if ($operation == 'view') {
+      // Anybody can view during creation.
+      if ($entity->isNew()) {
+        return AccessResult::allowed();
+      }
+
+      // Users with an active membership in the protocol can view.
+      $membership = Og::getMembership($entity, $account);
+      if ($membership) {
+        return AccessResult::allowed();
+      }
+
+      // Otherwise user must be a member of ALL owning communities to view.
+      foreach ($communities as $community) {
+        $membership = Og::getMembership($community, $account);
+        if (!$membership) {
+          return AccessResult::forbidden();
+        }
+      }
+
+      return AccessResult::forbidden();
+    }
+
+    // Not attached to communities yet.
+    // It's fine to update/delete.
+    if ($entity->isNew()) {
+      return AccessResult::allowed();
+    }
+
+    // If this protocol is attached to communities, user must have all
+    // relevant OG permissions for each.
+    if (!empty($communities)) {
+      $ogAccessService = \Drupal::service('og.access');
+      $result = AccessResult::allowed();
+
+      // Check each community.
+      foreach ($communities as $community) {
+        $result = $result->andIf($ogAccessService->userAccessGroupContentEntityOperation($operation, $community, $entity, $account));
+      }
+      return $result;
     }
 
     return AccessResult::forbidden();
@@ -35,8 +71,16 @@ class ProtocolAccessControlHandler extends EntityAccessControlHandler {
    * {@inheritdoc}
    */
   protected function checkCreateAccess(AccountInterface $account, array $context, $entity_bundle = NULL) {
-    return AccessResult::allowedIfHasPermission($account, 'add protocol entities');
-  }
+    $memberships = Og::getMemberships($account);
 
+    foreach ($memberships as $membership) {
+      if ($membership->getGroupEntityType() == 'community') {
+        if ($membership->hasPermission("create protocol protocol")) {
+          return AccessResult::allowed();
+        }
+      }
+    }
+    return AccessResult::forbidden();
+  }
 
 }
