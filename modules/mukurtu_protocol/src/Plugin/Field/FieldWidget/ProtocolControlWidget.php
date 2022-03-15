29c4821b41df2cd88143db76e62034c145c10151
Steve Taylor <steve_taylor@wsu.edu>
2022-03-14 17:13:24 -0700

    Widget is sort of working

--- modules/mukurtu_protocol/src/Plugin/Field/FieldWidget/ProtocolControlWidget.php
+++ modules/mukurtu_protocol/src/Plugin/Field/FieldWidget/ProtocolControlWidget.php
@@ -6,6 +6,7 @@ use Drupal\Core\Field\WidgetBase;
 use Drupal\Core\Field\FieldItemListInterface;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Field\FieldDefinitionInterface;
+use Drupal\mukurtu_protocol\Entity\ProtocolControl;
 use Exception;
 
 /**
@@ -23,41 +24,62 @@ use Exception;
 class ProtocolControlWidget extends WidgetBase {
 
   /**
+   * Helper function to put the values into the protocol control entity.
+   */
+  protected function syncProtocolControlValues($values, $pcEntity) {
+    // Privacy Setting.
+    $field_privacy_setting = $values[0]['field_sharing_setting'] ?? NULL;
+    if ($field_privacy_setting) {
+      $pcEntity->setPrivacySetting($field_privacy_setting);
+    }
+
+    // Protocols.
+    $protocols = $values[0]['field_protocols'] ?? NULL;
+    if ($protocols) {
+      $protocols = array_column($protocols, 'target_id');
+      $pcEntity->setProtocols($protocols);
+    }
+
+    return $pcEntity;
+  }
+
+  /**
    * Custom submit handler for the protocol control widget.
    */
   public static function customSubmit(array $form, FormStateInterface $form_state) {
-   // $dump = print_r($form_state->getValue('field_protocol_control'), TRUE);
-   //dpm("here first");
     $needSave = FALSE;
     $protocolControlValues = $form_state->getValue('field_protocol_control')[0] ?? NULL;
 
     /**
      * @var \Drupal\mukurtu_protocol\Entity\ProtocolControlInterface $entity
      */
-    $entity = $form_state->get('protocol_control_entity');
+    $pcEntity = $form_state->get('protocol_control_entity');
+    $entity = $form_state->getFormObject()->getEntity();
+
 
     // If we have both a protocol control entity and some new user given values,
     // set the new values as needed and update the protocol control entity.
-    if ($entity && $protocolControlValues) {
+    if ($pcEntity && $protocolControlValues) {
       // Privacy Setting.
       $field_privacy_setting = $protocolControlValues['field_sharing_setting'] ?? NULL;
-      if ($field_privacy_setting && $entity->getPrivacySetting() != $field_privacy_setting) {
-        $entity->setPrivacySetting($field_privacy_setting);
+      if ($field_privacy_setting && $pcEntity->getPrivacySetting() != $field_privacy_setting) {
+        $pcEntity->setPrivacySetting($field_privacy_setting);
         $needSave = TRUE;
       }
 
       // Protocols.
       $protocols = $protocolControlValues['field_protocols'] ?? NULL;
       $protocols = array_column($protocols, 'target_id');
-      if (!empty(array_diff($protocols, $entity->getProtocols()))) {
-        $entity->setProtocols($protocols);
+      if (!empty(array_diff($protocols, $pcEntity->getProtocols()))) {
+        $pcEntity->setProtocols($protocols);
         $needSave = TRUE;
       }
 
       // Save the protocol control entity if data was altered.
       if ($needSave) {
         try {
-          $entity->save();
+          $pcEntity->setControlledEntity($entity);
+          $pcEntity->save();
         }
         catch (Exception $e) {
           // Can't use DI because we're in a static method.
@@ -72,6 +94,7 @@ class ProtocolControlWidget extends WidgetBase {
    */
   public function formElement(FieldItemListInterface $items, $delta, array $element, array &$form, FormStateInterface $form_state) {
     $item = $items->get($delta);
+    $entity = $items->getEntity();
 
     $element = [
       '#type' => 'details',
@@ -80,36 +103,39 @@ class ProtocolControlWidget extends WidgetBase {
     ] + $element;
 
     /**
-     * @var \Drupal\mukurtu_protocol\Entity\ProtocolControlInterface $entity
+     * @var \Drupal\mukurtu_protocol\Entity\ProtocolControlInterface $pcEntity
      */
-    $entity = $item->entity;
-    if (is_null($entity) || !$entity->access('update')) {
+    $pcEntity = $item->entity;
+    if (is_null($pcEntity)) {
+      // Create a new PCE if we need one.
+      $name = "{$entity->getEntityTypeId()}:{$entity->uuid()}";
+      $pcEntity = ProtocolControl::create([
+        'name' => $name,
+        'field_sharing_setting' => 'all',
+      ]);
+      $pcEntity->setControlledEntity($entity);
+    }
+
+    if (is_null($pcEntity) || !$pcEntity->access('update')) {
       return [];
     }
-    //dpm("widget: " . ($entity->id() ?? $entity->uuid()));
 
     // Keep the protocol control entity for this item in the form.
     // if the user changes any of its fields in this widget we can
     // update it in ::customSubmit.
-    $form_state->set('protocol_control_entity', $entity);
+    $form_state->set('protocol_control_entity', $pcEntity);
 
     $element['target_id'] = [
       '#type' => 'hidden',
-      '#value' => $entity->id() ?? $entity->uuid(),
+      '#element_validate' => [
+        [static::class, 'validate'],
+      ],
+      '#default_value' => $pcEntity->id() ?? $pcEntity->uuid(),
     ];
-
-    // this worked.
- /*    $element['field_privacy_setting'] = [
-      '#type' => 'radios',
-      '#options' => $entity->getSharingSettingOptions(),
-      '#default_value' => $entity->getSharingSetting(),
-    ]; */
-
 
     /** @var \Drupal\Core\Entity\Display\EntityFormDisplayInterface $form_display */
     $form_display = \Drupal::entityTypeManager()->getStorage('entity_form_display')->load('protocol_control.protocol_control.default');
     $fieldnames = ['field_protocols', 'field_sharing_setting'];
-    //$name = 'field_privacy_setting';
     foreach ($fieldnames as $name) {
       /**
        * @var \Drupal\Core\Field\PluginSettingsInterface $widget
@@ -117,7 +143,7 @@ class ProtocolControlWidget extends WidgetBase {
       $widget = $form_display->getRenderer($name);
 
       /** @var \Drupal\Core\Field\EntityReferenceFieldItemList $items */
-      $items = $entity->get($name);
+      $items = $pcEntity->get($name);
 
       $items->filterEmptyItems();
       /** @var \Drupal\Core\Field\Plugin\Field\FieldWidget\OptionsButtonsWidget $widget */
@@ -136,38 +162,7 @@ class ProtocolControlWidget extends WidgetBase {
       $element[$name] = $mockedWidget['widget'];
       unset($element[$name]['#parents']);
     }
-/*     $element[$name]['#type'] = $mockedWidget['widget']['#type'];
-    $element[$name]['#options'] = $mockedWidget['widget']['#options'];
-    $element[$name]['#default_value'] = $mockedWidget['widget']['#default_value']; */
-    //unset($element[$name]['#key_column']);
-    //dpm($element[$name]);
 
-    //$fields = $entity->getFields();
-    //dpm(array_keys($fields));
-
-    //$element['test'] = $fields['field_privacy_setting']->
-    //$element['field_protocols'] = $fields['field_protocols']->view();
-/* $field_name = $this->fieldDefinition->getFieldStorageDefinition()->getName();
-    $wrapper = 'edit-' . str_replace('_', '-', $field_name) . '-ajax-wrapper';
-
-    $referenced_entities = $items->referencedEntities();
-
-    $element += [
-      '#type' => 'select',
-      '#default_value' => isset($referenced_entities[$delta]) ? $referenced_entities[$delta]->id() : -1,
-      '#options' => $this->getElementOptions($items, $delta, $element, $form, $form_state),
-      '#element_validate' => [
-        [static::class, 'validate'],
-      ],
-      '#ajax' => [
-        'event' => 'change',
-        'callback' => [$this, 'protocolChangeCallback'],
-        'wrapper' => $wrapper,
-      ],
-    ]; */
-
-
-    // dpm($element);
     return $element;
   }
 
@@ -175,24 +170,25 @@ class ProtocolControlWidget extends WidgetBase {
    * {@inheritdoc}
    */
   public function massageFormValues(array $values, array $form, FormStateInterface $form_state) {
-    $entity = $form_state->getformObject()->getEntity();
     $pcEntity = $form_state->get('protocol_control_entity');
 
-    // If both the entity and PCE are new, we need to save the PCE
-    // and change the reference to use the PCE ID.
-    if ($entity->isNew() && $pcEntity->isNew()) {
+    // Copy the widget values over to the PCE.
+    $pcEntity = $this->syncProtocolControlValues($values, $pcEntity);
+
+    // If the PCE is new, save it and change the reference to use the ID.
+    if ($pcEntity->isNew()) {
       try {
         // Save the PCE.
-        $pcEntity->setControlledEntity($entity);
         $pcEntity->save();
-
-        // Alter the entity reference to use the ID.
-        $values[0]['target_id'] = $pcEntity->id();
+        $form_state->set('protocol_control_entity', $pcEntity);
       }
       catch (Exception $e) {
         // Blank.
       }
     }
+
+    // Alter the entity reference to use the ID.
+    $values[0]['target_id'] = $pcEntity->id();
 
     return $values;
   }
@@ -205,6 +201,13 @@ class ProtocolControlWidget extends WidgetBase {
       return TRUE;
     }
     return FALSE;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public static function validate($element, FormStateInterface $form_state) {
+    $value = $element['#value'];
   }
 
 }
