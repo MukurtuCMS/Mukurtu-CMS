016978e31f208862e21495867c5d15e83dd69845
Steve Taylor <steve_taylor@wsu.edu>
2022-03-28 12:56:40 -0700

    Placeholder functionality to add users to protocol

--- modules/mukurtu_protocol/src/Form/ProtocolAddForm.php
+++ modules/mukurtu_protocol/src/Form/ProtocolAddForm.php
@@ -27,6 +27,20 @@ class ProtocolAddForm extends EntityForm {
    */
   protected $community;
 
+  /**
+   * The user IDs of the protocol stewards.
+   *
+   * @var int[]
+   */
+  protected $protocolStewards;
+
+  /**
+   * The user IDs of the protocol members.
+   *
+   * @var int[]
+   */
+  protected $protocolMembers;
+
   protected $moduleHandler;
 
 
@@ -39,6 +53,7 @@ class ProtocolAddForm extends EntityForm {
 
   public function __construct() {
     $this->entity = Protocol::create([]);
+
   }
 
   /**
@@ -49,6 +64,7 @@ class ProtocolAddForm extends EntityForm {
     $instance = parent::create($container);
     $instance->account = $container->get('current_user');
     $instance->moduleHandler = $container->get('module_handler');
+    $instance->entityTypeManager = $container->get('entity_type.manager');
     return $instance;
   }
 
@@ -60,6 +76,7 @@ class ProtocolAddForm extends EntityForm {
 
     // Set the community relationship.
     if ($community) {
+      $this->community = $community;
       $this->entity->setCommunities([$community]);
     }
 
@@ -94,14 +111,23 @@ class ProtocolAddForm extends EntityForm {
     ];
 
 
-    // Community Managers.
-/*     $form['community_managers'] = [
+    // Protocol Stewards.
+    $form['protocol_stewards'] = [
       '#type' => 'entity_autocomplete',
-      '#title' => $this->t('Community Managers'),
-      '#description' => $this->t('Helper text about community managers.'),
+      '#title' => $this->t('Protocol Stewards'),
+      '#description' => $this->t('Helper text about protocol stewards.'),
       '#target_type' => 'user',
       '#selection_handler' => 'default',
-    ]; */
+    ];
+
+    // Protocol Members.
+    $form['protocol_members'] = [
+      '#type' => 'entity_autocomplete',
+      '#title' => $this->t('Members'),
+      '#description' => $this->t('Helper text about protocol membership.'),
+      '#target_type' => 'user',
+      '#selection_handler' => 'default',
+    ];
 
     return $form;
   }
@@ -110,15 +136,27 @@ class ProtocolAddForm extends EntityForm {
    * {@inheritdoc}
    */
   protected function actions(array $form, FormStateInterface $form_state) {
-    $actions['submit'] = [
+    $actions['submit_another'] = [
       '#type' => 'submit',
       '#value' => $this
-        ->t('Create Protocol'),
+        ->t('Save and create another'),
       '#submit' => [
         '::submitForm',
         '::save',
       ],
     ];
+
+    $actions['submit_done'] = [
+      '#type' => 'submit',
+      '#value' => $this
+        ->t('Save and view community'),
+      '#submit' => [
+        '::submitForm',
+        '::save',
+        '::redirectToCommunity',
+      ],
+    ];
+
     return $actions;
   }
 
@@ -132,7 +170,11 @@ class ProtocolAddForm extends EntityForm {
     $entity->setDescription($form_state->getValue('field_description'));
     $entity->setSharingSetting($form_state->getValue('field_access_mode'));
 
-    // @todo set the memberships for the protocol.
+    // Set the memberships for the protocol.
+    $stewards = $form_state->getValue('protocol_stewards');
+    $this->protocolStewards = [$stewards];
+    $members = $form_state->getValue('protocol_members');
+    $this->protocolMembers = [$members];
 
     return $entity;
   }
@@ -142,18 +184,34 @@ class ProtocolAddForm extends EntityForm {
    */
   public function save(array $form, FormStateInterface $form_state) {
     if ($this->entity->save()) {
-      /** @var \Drupal\mukurtu_protocol\Entity\Protocol $community */
+      /** @var \Drupal\mukurtu_protocol\Entity\Protocol $protocol */
       $protocol = $this->entity;
 
-      // @todo Add community managers here.
-      /** @var \Drupal\core\Session\AccountInterface[] $managers */
-/*       $managers = $this->entityTypeManager->getStorage('user')->loadMultiple($this->communityManagers);
-      foreach ($managers as $manager) {
-        $community->addMember($manager, ['community_manager']);
-      } */
+      // Add protocol stewards.
+      /** @var \Drupal\core\Session\AccountInterface[] $stewards */
+      if (!empty($this->protocolStewards)) {
+        $stewards = $this->entityTypeManager->getStorage('user')->loadMultiple($this->protocolStewards);
+        foreach ($stewards as $steward) {
+          $protocol->addMember($steward)->setRoles($steward, ['protocol_steward']);
+        }
+      }
 
-      //$form_state->setRedirect('entity.community.canonical', ['community' => $this->community->id()]);
+      // Add protocol members.
+      /** @var \Drupal\core\Session\AccountInterface[] $members */
+      if (!empty($this->protocolMembers)) {
+        $members = $this->entityTypeManager->getStorage('user')->loadMultiple($this->protocolMembers);
+        foreach ($members as $member) {
+          $protocol->addMember($member);
+        }
+      }
     }
+  }
+
+  /**
+   * Redirect to the owning community after save.
+   */
+  public function redirectToCommunity(array $form, FormStateInterface $form_state) {
+    $form_state->setRedirect('entity.community.canonical', ['community' => $this->community->id()]);
   }
 
 }
