299ec4866fefce983fe7f49f937a65f3198e91f7
Steve Taylor <steve_taylor@wsu.edu>
2022-04-08 16:34:24 -0700

    Exclude already selected users

--- modules/mukurtu_protocol/src/Plugin/EntityBrowser/Widget/MukurtuUserSelection.php
+++ modules/mukurtu_protocol/src/Plugin/EntityBrowser/Widget/MukurtuUserSelection.php
@@ -44,12 +44,15 @@ class MukurtuUserSelection extends WidgetBase implements ContainerFactoryPluginI
    * @param string $match_operator
    *   (Optional) The operation the matching should be done with. Defaults
    *   to "CONTAINS".
+   * @param mixed $exclude
+   *   (Optional) The array of UIDs to exclude from the query. Defaults to
+   *   an empty array.
    *
    * @return \Drupal\Core\Entity\Query\QueryInterface
    *   The EntityQuery object with the basic conditions and sorting applied to
    *   it.
    */
-  protected function buildEntityQuery($match = NULL, $match_operator = 'CONTAINS') {
+  protected function buildEntityQuery($match = NULL, $match_operator = 'CONTAINS', $exclude = []) {
     $query = $this->entityTypeManager->getStorage('user')->getQuery();
     $query->accessCheck(TRUE);
     $query->condition('uid', 0, '<>');
@@ -61,6 +64,11 @@ class MukurtuUserSelection extends WidgetBase implements ContainerFactoryPluginI
         ->condition('name', $match, $match_operator)
         ->condition('mail', $match, $match_operator);
       $query->condition($conditionGroup);
+    }
+
+    // Exclude already selected.
+    if (!empty($exclude)) {
+      $query->condition('uid', $exclude, 'NOT IN');
     }
 
     // Get the group object (community/protocol).
@@ -115,7 +123,13 @@ class MukurtuUserSelection extends WidgetBase implements ContainerFactoryPluginI
       '#default_value' => '',
     ];
 
-    $query = $this->buildEntityQuery($form_state->getValue('search'));
+    // Get the already selected users so we can remove them from the query.
+    $storage = $form_state->getStorage();
+    $selected = $storage['entity_browser']['selected_entities'] ?? [];
+    $excludeUIDs = array_map(fn($user) => $user->id(), $selected);
+
+    // Build the query for the user selection.
+    $query = $this->buildEntityQuery($form_state->getValue('search'), 'CONTAINS', $excludeUIDs);
     $results = $query->execute();
 
     $users = $this->entityTypeManager->getStorage('user')->loadMultiple($results);
