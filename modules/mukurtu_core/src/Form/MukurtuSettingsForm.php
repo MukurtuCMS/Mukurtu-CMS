9d4d416e7f282a1c6d52310777316aa4f5110df0
Steve Taylor <steve_taylor@wsu.edu>
2022-06-13 10:28:24 -0700

    49 - Related Content field changes

--- modules/mukurtu_core/src/Form/MukurtuSettingsForm.php
+++ modules/mukurtu_core/src/Form/MukurtuSettingsForm.php
@@ -47,12 +47,25 @@ class MukurtuSettingsForm extends ConfigFormBase {
       $default_media = NULL;
     }
 
+    $defaultRelatedContentOption = $config->get('mukurtu_related_content_display') ?? 'computed';
+
     $form['mukurtu_default_image'] = [
       '#title' => 'Default Image',
       '#description' => $this->t('This image will be used when media is not available for a field or item.'),
       '#type'          => 'entity_autocomplete',
       '#target_type'   => 'media',
       '#default_value' => $default_media,
+    ];
+
+    $form['mukurtu_related_content_display'] = [
+      '#title' => 'Related Content Display',
+      '#description' => $this->t('Select what content should be displayed for the related content field.'),
+      '#type' => 'radios',
+      '#default_value' => $defaultRelatedContentOption,
+      '#options' => [
+        'localonly' => $this->t('Display value of the item\'s related content field as is.'),
+        'computed' => $this->t('Display value of the item\'s related content field, but also include content that includes this item as related content.'),
+      ],
     ];
 
     return parent::buildForm($form, $form_state);
@@ -64,6 +77,7 @@ class MukurtuSettingsForm extends ConfigFormBase {
   public function submitForm(array &$form, FormStateInterface $form_state) {
     $this->configFactory->getEditable(static::SETTINGS)
       ->set('mukurtu_default_image', $form_state->getValue('mukurtu_default_image'))
+      ->set('mukurtu_related_content_display', $form_state->getValue('mukurtu_related_content_display'))
       ->save();
 
     parent::submitForm($form, $form_state);
---
+++ modules/mukurtu_core/src/Plugin/Field/AllRelatedContentItemList.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Drupal\mukurtu_core\Plugin\Field;
+
+use Drupal\Core\Field\EntityReferenceFieldItemList;
+use Drupal\Core\TypedData\ComputedItemListTrait;
+
+/**
+ * AllRelatedContentItemList class to implement 'All Related Content' field.
+ */
+class AllRelatedContentItemList extends EntityReferenceFieldItemList {
+  use ComputedItemListTrait;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function computeValue() {
+    $config = \Drupal::config('mukurtu.settings');
+    $defaultRelatedContentOption = $config->get('mukurtu_related_content_display') ?? 'computed';
+    $entity = $this->getEntity();
+
+    // Get the local refs for the related content field, if present.
+    $relatedIds = [];
+    $delta = 0;
+    if ($entity->hasField('field_related_content')) {
+      $related = $entity->get('field_related_content')->referencedEntities();
+      foreach ($related as $relatedContent) {
+        $this->list[$delta] = $this->createItem($delta++, $relatedContent->id());
+
+        // Track IDs of content we've already added so we don't dupe them later.
+        $relatedIds[] = $relatedContent->id();
+      }
+    }
+
+    // If site is configured to display the related content field as is,
+    // we are done at this point.
+    if ($defaultRelatedContentOption == 'localonly') {
+      return;
+    }
+
+    // Query for other content referencing this entity and add them to the
+    // related content display.
+    $query = \Drupal::entityQuery('node')
+      ->condition('field_related_content', $entity->id())
+      ->condition('status', TRUE)
+      ->accessCheck(FALSE);
+    $results = $query->execute();
+
+    foreach ($results as $relatedId) {
+      // Don't add duplicates.
+      if (!in_array($relatedId, $relatedIds)) {
+        $this->list[$delta] = $this->createItem($delta++, $relatedId);
+        $relatedIds[] = $relatedId;
+      }
+    }
+  }
+
+}
