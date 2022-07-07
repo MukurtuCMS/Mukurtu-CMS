24b3dcbce60fadc8cd59f150946601cec6a3ac8a
Steve Taylor <steve_taylor@wsu.edu>
2022-07-07 09:21:11 -0700

    Tweak CR title handling

--- modules/mukurtu_community_records/js/mukurtu-community-records.js
+++ modules/mukurtu_community_records/js/mukurtu-community-records.js
@@ -3,7 +3,7 @@
     attach: function (context, settings) {
       $(document).ready(function () {
         $('.community-records details.horizontal-tabs-pane', context).drupalSetSummary(function (context) {
-          let title = $(context).find('.field--name-title').html();
+          let title = $(context).find('.field--name-title').html() ?? $(document).find('.page-title').html();
           return title;
         });
       });
