f1357723ee32f6e448382d198908e21016ed67ad
nick-deer <nicolette.dixon@wsu.edu>
2023-02-15 16:30:58 -0800

    finished collection bundle field def

--- modules/mukurtu_collection/src/Entity/Collection.php
+++ modules/mukurtu_collection/src/Entity/Collection.php
@@ -7,8 +7,245 @@ use Drupal\Core\Entity\EntityInterface;
 use Drupal\mukurtu_collection\Entity\CollectionInterface;
 use Drupal\Core\Cache\Cache;
 use Drupal\Core\Entity\EntityStorageInterface;
+use Drupal\Core\Entity\EntityTypeInterface;
+use Drupal\Core\Field\BaseFieldDefinition;
+use Drupal\mukurtu_protocol\CulturalProtocolControlledTrait;
+use Drupal\mukurtu_protocol\CulturalProtocolControlledInterface;
 
-class Collection extends Node implements CollectionInterface {
+class Collection extends Node implements CollectionInterface, CulturalProtocolControlledInterface {
+  use CulturalProtocolControlledTrait;
+
+  public static function bundleFieldDefinitions(EntityTypeInterface $entity_type, $bundle, array $base_field_definitions) {
+    $definitions = self::getProtocolFieldDefinitions();
+
+    $definitions['field_child_collections'] = BaseFieldDefinition::create('entity_reference')
+      ->setLabel(t('Sub-Collections'))
+      ->setDescription(t(''))
+      ->setSettings([
+        'target_type' => 'collection',
+        'handler' => 'default:node',
+        'handler_settings' => [
+          'target_bundles' => [
+            'collection' => 'collection'
+          ],
+          'sort' => [
+            'field' => 'title',
+            'direction' => 'ASC'
+          ],
+          'auto_create' => FALSE,
+          'auto_create_bundle' => '',
+        ]
+      ])
+      ->setDefaultValue('')
+      ->setCardinality(-1)
+      ->setRequired(FALSE)
+      ->setRevisionable(TRUE)
+      ->setTranslatable(FALSE)
+      ->setDisplayConfigurable('view', TRUE)
+      ->setDisplayConfigurable('form', TRUE);
+
+    $definitions['field_collection_image'] = BaseFieldDefinition::create('entity_reference')
+      ->setLabel(t('Image'))
+      ->setDescription(t(''))
+      ->setSettings([
+        'target_type' => 'media',
+        'handler' => 'default:media',
+        'handler_settings' => [
+          'target_bundles' => [
+            'image' => 'image'
+          ],
+          'sort' => [
+            'field' => '_none',
+            'direction' => 'ASC'
+          ],
+          'auto_create' => FALSE,
+          'auto_create_bundle' => '',
+        ]
+      ])
+      ->setDefaultValue('')
+      ->setCardinality(1)
+      ->setRequired(FALSE)
+      ->setRevisionable(TRUE)
+      ->setTranslatable(FALSE)
+      ->setDisplayConfigurable('view', TRUE)
+      ->setDisplayConfigurable('form', TRUE);
+
+    $definitions['field_collection_type'] = BaseFieldDefinition::create('list_string')
+      ->setLabel(t('Collection Type'))
+      ->setDescription(t(''))
+      ->setSettings([
+        'allowed_values' => [
+          '-' => [
+            'value' => 'default',
+            'label' => 'Default'
+          ],
+          '-' => [
+            'value' => 'multipage',
+            'label' => 'Multipage Item'
+          ]
+        ],
+        'allowed_values_function' => ''
+      ])
+      ->setDefaultValue('default')
+      ->setCardinality(1)
+      ->setRequired(TRUE)
+      ->setRevisionable(TRUE)
+      ->setTranslatable(FALSE)
+      ->setDisplayConfigurable('view', TRUE)
+      ->setDisplayConfigurable('form', TRUE);
+
+    $definitions['field_content_type'] = BaseFieldDefinition::create('string')
+      ->setLabel(t('Content Type'))
+      ->setDescription(t(''))
+      ->setSettings([
+        'max_length' => 255,
+      ])
+      ->setDefaultValue('')
+      ->setCardinality(1)
+      ->setRequired(FALSE)
+      ->setRevisionable(TRUE)
+      ->setTranslatable(TRUE)
+      ->setDisplayConfigurable('view', TRUE)
+      ->setDisplayConfigurable('form', TRUE);
+
+    $definitions['field_credit'] = BaseFieldDefinition::create('string')
+      ->setLabel(t('Credit'))
+      ->setDescription(t(''))
+      ->setSettings([
+        'max_length' => 255,
+      ])
+      ->setDefaultValue('')
+      ->setCardinality(1)
+      ->setRequired(FALSE)
+      ->setRevisionable(TRUE)
+      ->setTranslatable(FALSE)
+      ->setDisplayConfigurable('view', TRUE)
+      ->setDisplayConfigurable('form', TRUE);
+    $definitions['field_description'] = BaseFieldDefinition::create('text_with_summary')
+      ->setLabel(t('Description'))
+      ->setDescription(t(''))
+      ->setSettings([
+        'display_summary' => TRUE,
+        'required_summary' => FALSE,
+      ])
+      ->setDefaultValue('')
+      ->setCardinality(1)
+      ->setRequired(FALSE)
+      ->setRevisionable(TRUE)
+      ->setTranslatable(FALSE)
+      ->setDisplayConfigurable('view', TRUE)
+      ->setDisplayConfigurable('form', TRUE);
+    $definitions['field_items_in_collection'] = BaseFieldDefinition::create('entity_reference')
+      ->setLabel(t('Items in Collection'))
+      ->setDescription(t(''))
+      ->setSettings([
+        'target_type' => 'node',
+        'handler' => 'default:node',
+        'handler_settings' => [
+          'target_bundles' => NULL,
+          'sort' => [
+            'field' => 'title',
+            'direction' => 'ASC'
+          ],
+          'auto_create' => FALSE,
+          'auto_create_bundle' => 'collection',
+        ]
+      ])
+      ->setDefaultValue('')
+      ->setCardinality(1)
+      ->setRequired(FALSE)
+      ->setRevisionable(TRUE)
+      ->setTranslatable(FALSE)
+      ->setDisplayConfigurable('view', TRUE)
+      ->setDisplayConfigurable('form', TRUE);
+    $definitions['field_keywords'] = BaseFieldDefinition::create('entity_reference')
+      ->setLabel(t('Keywords'))
+      ->setDescription(t('Keywords provide added ways to group your content. They make it easier for users to search and retrieve content.'))
+      ->setSettings([
+        'target_type' => 'taxonomy_term',
+        'handler' => 'default:taxonomy_term',
+        'handler_settings' => [
+          'target_bundles' => [
+            'keywords' => 'keywords'
+          ],
+          'sort' => [
+            'field' => 'name',
+            'direction' => 'asc'
+          ],
+          'auto_create' => TRUE,
+          'auto_create_bundle' => '',
+        ]
+      ])
+      ->setDefaultValue('')
+      ->setCardinality(-1)
+      ->setRequired(FALSE)
+      ->setRevisionable(TRUE)
+      ->setTranslatable(TRUE)
+      ->setDisplayConfigurable('view', TRUE)
+      ->setDisplayConfigurable('form', TRUE);
+    $definitions['field_parent_collection'] = BaseFieldDefinition::create('entity_reference')
+      ->setLabel(t('Parent Collection'))
+      ->setDescription(t(''))
+      ->setSettings([
+        'target_type' => 'node',
+        'handler' => 'default:node',
+        'handler_settings' => [
+          'target_bundles' => [
+            'collection' => 'collection'
+          ],
+          'sort' => [
+            'field' => 'title',
+            'direction' => 'ASC'
+          ],
+          'auto_create' => FALSE,
+          'auto_create_bundle' => '',
+        ]
+      ])
+      ->setDefaultValue('')
+      ->setCardinality(1)
+      ->setRequired(FALSE)
+      ->setRevisionable(TRUE)
+      ->setTranslatable(FALSE)
+      ->setDisplayConfigurable('view', TRUE)
+      ->setDisplayConfigurable('form', TRUE);
+    $definitions['field_related_content'] = BaseFieldDefinition::create('entity_reference')
+      ->setLabel(t('Related Content'))
+      ->setDescription(t(''))
+      ->setSettings([
+        'target_type' => 'node',
+        'handler' => 'default:node',
+        'handler_settings' => [
+          'target_bundles' => NULL,
+          'sort' => [
+            'field' => '_none'
+          ],
+          'auto_create' => FALSE,
+          'auto_create_bundle' => 'article',
+        ]
+      ])
+      ->setDefaultValue('')
+      ->setCardinality(-1)
+      ->setRequired(FALSE)
+      ->setRevisionable(TRUE)
+      ->setTranslatable(TRUE)
+      ->setDisplayConfigurable('view', TRUE)
+      ->setDisplayConfigurable('form', TRUE);
+    $definitions['field_summary'] = BaseFieldDefinition::create('string')
+      ->setLabel(t('Summary'))
+      ->setDescription(t(''))
+      ->setSettings([
+        'max_length' => 255,
+      ])
+      ->setDefaultValue('')
+      ->setCardinality(1)
+      ->setRequired(FALSE)
+      ->setRevisionable(TRUE)
+      ->setTranslatable(TRUE)
+      ->setDisplayConfigurable('view', TRUE)
+      ->setDisplayConfigurable('form', TRUE);
+
+    return $definitions;
+  }
 
   /**
    * {@inheritdoc}
@@ -154,5 +391,4 @@ class Collection extends Node implements CollectionInterface {
     // Invalid the collection's cache as well.
     Cache::invalidateTags($this->getCacheTagsToInvalidate());
   }
-
 }
