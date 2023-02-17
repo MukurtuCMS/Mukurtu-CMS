b8f72711df1376515a6e80653c36af489f65a941
nick-deer <nicolette.dixon@wsu.edu>
2023-02-17 15:35:09 -0800

    finished audio bundle field def and deleted config

---
+++ modules/mukurtu_media/src/Entity/Audio.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Drupal\mukurtu_media\Entity;
+
+use Drupal\Core\Entity\EntityStorageInterface;
+use Drupal\media\Entity\Media;
+use Drupal\mukurtu_media\Entity\AudioInterface;
+use Drupal\Core\Field\BaseFieldDefinition;
+use Drupal\Core\Entity\EntityTypeInterface;
+use Drupal\mukurtu_protocol\CulturalProtocolControlledTrait;
+use Drupal\mukurtu_protocol\CulturalProtocolControlledInterface;
+
+/**
+ * Defines the Audio media entity bundle class.
+ */
+class Audio extends Media implements AudioInterface, CulturalProtocolControlledInterface {
+
+  use CulturalProtocolControlledTrait;
+
+  public static function bundleFieldDefinitions(EntityTypeInterface $entity_type, $bundle, array $base_field_definitions) {
+    $definitions = self::getProtocolFieldDefinitions();
+
+    $definitions['field_media_audio_file'] = BaseFieldDefinition::create('file')
+      ->setLabel(t('Audio file'))
+      ->setDescription(t(''))
+      ->setDefaultValue('')
+      ->setSettings([
+        'target_type' => 'file',
+        'handler' => 'default:file',
+        'file_directory' => '[date:custom:Y]-[date:custom:m]',
+        'file_extensions' => 'mp3 wav aac',
+        'max_filesize' => '',
+        'description_field' => FALSE,
+        'display_field' => FALSE,
+        'display_default' => FALSE,
+        'uri_scheme' => 'private',
+      ])
+      ->setCardinality(1)
+      ->setRequired(TRUE)
+      ->setRevisionable(TRUE)
+      ->setTranslatable(TRUE)
+      ->setDisplayConfigurable('view', TRUE)
+      ->setDisplayConfigurable('form', TRUE);
+
+    $definitions['field_transcription'] = BaseFieldDefinition::create('text_long')
+      ->setLabel(t('Transcription'))
+      ->setDescription(t(''))
+      ->setDefaultValue('')
+      ->setCardinality(1)
+      ->setRequired(FALSE)
+      ->setRevisionable(TRUE)
+      ->setTranslatable(FALSE)
+      ->setDisplayConfigurable('view', TRUE)
+      ->setDisplayConfigurable('form', TRUE);
+
+    return $definitions;
+  }
+}
