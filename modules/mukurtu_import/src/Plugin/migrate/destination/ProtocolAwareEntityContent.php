bbb29e1017526bf1cf7c4f9e206f71858e6db8de
Steve Taylor <steve_taylor@wsu.edu>
2023-03-17 11:12:11 -0700

    Remove account switching for validation on import

--- modules/mukurtu_import/src/Plugin/migrate/destination/ProtocolAwareEntityContent.php
+++ modules/mukurtu_import/src/Plugin/migrate/destination/ProtocolAwareEntityContent.php
@@ -9,6 +9,8 @@ use Drupal\migrate\Plugin\MigrateIdMapInterface;
 use Drupal\Core\Entity\ContentEntityInterface;
 use Drupal\mukurtu_protocol\CulturalProtocolControlledInterface;
 use Drupal\Core\Entity\RevisionLogInterface;
+use Drupal\Core\Entity\FieldableEntityInterface;
+use Drupal\migrate\Exception\EntityValidationException;
 
 class ProtocolAwareEntityContent extends EntityContentBase {
   public function import(Row $row, array $old_destination_id_values = []) {
@@ -40,6 +42,25 @@ class ProtocolAwareEntityContent extends EntityContentBase {
       $ids[] = $entity->language()->getId();
     }
     return $ids;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateEntity(FieldableEntityInterface $entity) {
+    // EntityContentBase uses the accountSwitcher to switch to the
+    // owner account. We don't want to do that. For the Mukurtu
+    // importer the user doing the import is the content creator
+    // and all checks should be run using their account.
+    try {
+      $violations = $entity->validate();
+    } finally {
+      // Intentionally left blank.
+    }
+
+    if (count($violations) > 0) {
+      throw new EntityValidationException($violations);
+    }
   }
 
   /**
