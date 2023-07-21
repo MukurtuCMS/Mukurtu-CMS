97099a3d557989a1be20a3b549e66fc5d4213a5a
nick-deer <nicolette.dixon@wsu.edu>
2023-07-21 13:21:46 -0700

    changed export form for empty export list

--- modules/mukurtu_export/src/Form/ExportItemAndFormatSelectionForm.php
+++ modules/mukurtu_export/src/Form/ExportItemAndFormatSelectionForm.php
@@ -4,6 +4,7 @@ namespace Drupal\mukurtu_export\Form;
 
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\mukurtu_export\Form\ExportBaseForm;
+use Drupal\Core\Url;
 
 /**
  * Export Item Summary and Format Selection form.
@@ -21,6 +22,15 @@ class ExportItemAndFormatSelectionForm extends ExportBaseForm {
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
+    // Assume export list is empty until we come across an item.
+    $exportListIsEmpty = TRUE;
+    foreach ($this->source->getEntities() as $entityType => $entities) {
+      if (!empty($entities)) {
+        $exportListIsEmpty = FALSE;
+        break;
+      }
+    }
+
     $form['export_list']['content'] = [
       '#type' => 'view',
       '#name' => 'export_list_content',
@@ -35,21 +45,32 @@ class ExportItemAndFormatSelectionForm extends ExportBaseForm {
       '#embed' => TRUE,
     ];
 
-    $form['export_list']['exporter'] = [
-      '#type' => 'select',
-      '#title' => 'Select Export Format',
-      '#default_value' => $this->getExporterId(),
-      '#options' => $this->getExporterOptions(),
-    ];
+    // Only show this if export list is not empty
+    if (!$exportListIsEmpty) {
+      $form['export_list']['exporter'] = [
+        '#type' => 'select',
+        '#title' => 'Select Export Format',
+        '#default_value' => $this->getExporterId(),
+        '#options' => $this->getExporterOptions(),
+      ];
 
-    $form['actions'] = [
-      '#type' => 'actions',
-    ];
-    $form['actions']['submit'] = [
-      '#type' => 'submit',
-      '#value' => $this->t('Next - Configure Export'),
-      '#button_type' => 'primary',
-    ];
+      $form['actions'] = [
+        '#type' => 'actions',
+      ];
+      $form['actions']['submit'] = [
+        '#type' => 'submit',
+        '#value' => $this->t('Next - Configure Export'),
+        '#button_type' => 'primary',
+      ];
+    }
+    else {
+      // Alternate form display if export list is empty.
+      $manageContentMarkup = '<a href="' . url::fromRoute("view.mukurtu_manage_all_content.mukurtu_manage_content")->toString() . '">Manage Content</a>';
+      $browseContentMarkup = '<a href="' . url::fromRoute("mukurtu_browse.browse_page")->toString() . '">Browse</a>';
+      $form['message'] = [
+        '#markup' => 'You have no items marked for export. Please visit ' . $manageContentMarkup . ' or ' . $browseContentMarkup . ' to add items for export.',
+      ];
+    }
 
     return $form;
   }
