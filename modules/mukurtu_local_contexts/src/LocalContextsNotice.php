bff0b34a70df43476db6575dd6be54598d58413c
nick-deer <nicolette.dixon@wsu.edu>
2023-08-30 15:30:01 -0700

    Index translations in the templates by locale

--- modules/mukurtu_local_contexts/src/LocalContextsNotice.php
+++ modules/mukurtu_local_contexts/src/LocalContextsNotice.php
@@ -41,9 +41,49 @@ class LocalContextsNotice extends LocalContextsHubBase
       ->fields('t', ['locale', 'language', 'name', 'text']);
     $tResult = $tQuery->execute();
 
+    $bookkeep = [];
+
     // Load every translation.
     while ($translationInfo = $tResult->fetchAssoc()) {
-      $this->translations[] = $translationInfo;
+      // Bookkeep the number of locales there are (including where locale is
+      // null).
+
+      // Must determine the proper index to store each translation at, since we
+      // could have no locales or multiple translations per locale.
+      $translationIndex = '';
+
+      // Handle translations with no or null locale.
+      if (!isset($translationInfo['locale']) || $translationInfo['locale'] == '') {
+        // Check if the bookkeep is tracking translations with no locale yet.
+        if (!isset($bookkeep['no_locale_count'])) {
+          // If it isn't, initialize the no_locale_count key to 1.
+          $bookkeep['no_locale_count'] = 1;
+        }
+        else {
+          // If it is, increment the no locale count.
+          $bookkeep['no_locale_count']++;
+        }
+
+        // Now set the translation index to the no locale count.
+        $translationIndex = strval($bookkeep['no_locale_count']);
+      }
+
+      // Handle translations with locales.
+      else {
+        // Check if this locale exists in the bookkeep.
+        if (!isset($bookkeep[$translationInfo['locale']])) {
+          // If not, initialize its count to 1.
+          $bookkeep[$translationInfo['locale']] = 1;
+        }
+        else {
+          // If so, increment its count by 1.
+          $bookkeep[$translationInfo['locale']]++;
+        }
+        // Generate the translation index but add its count to the end so it's
+        // unique.
+        $translationIndex = $translationInfo['locale'] . '-' . $bookkeep[$translationInfo['locale']];
+      }
+      $this->translations[$translationIndex] = $translationInfo;
     }
   }
 }
