1e3b942e855ec26ea9e9221d4bc2b7d65cf33e24
nick-deer <nicolette.dixon@wsu.edu>
2023-09-20 12:04:49 -0700

    Removed unnecessary method

--- modules/mukurtu_core/src/Plugin/media/Source/ExternalEmbed.php
+++ modules/mukurtu_core/src/Plugin/media/Source/ExternalEmbed.php
@@ -22,21 +22,5 @@ class ExternalEmbed extends MediaSourceBase
   /**
    * {@inheritdoc}
    */
-  public function getMetadataAttributes()
-  {
-    return [
-      'embed_code' => $this->t('Embed code'),
-    ];
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function getMetadata(MediaInterface $media, $attribute_name)
-  {
-    // Get the text_long field where the embed markup is stored
-    $remote_field = $media->get($this->configuration['source_field']);
-    $json_arr = json_decode($remote_field->value);
-    return $json_arr->$attribute_name ?? parent::getMetadata($media, $attribute_name);
-  }
+  public function getMetadataAttributes(){}
 }
