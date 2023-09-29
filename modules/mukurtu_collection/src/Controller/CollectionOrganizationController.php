27abcbf909e57713d20a65ebacda395b278fe7fc
Steve Taylor <steve_taylor@wsu.edu>
2023-09-29 16:40:49 -0700

    Add new collections as subcollections

--- modules/mukurtu_collection/src/Controller/CollectionOrganizationController.php
+++ modules/mukurtu_collection/src/Controller/CollectionOrganizationController.php
@@ -2,17 +2,24 @@
 
 namespace Drupal\mukurtu_collection\Controller;
 
-use Drupal\Core\Controller\ControllerBase;
 use Drupal\Core\Access\AccessResult;
-use Drupal\node\NodeInterface;
+use Drupal\Core\Controller\ControllerBase;
 use Drupal\Core\Session\AccountInterface;
 use Drupal\mukurtu_collection\Entity\Collection;
+use Drupal\node\NodeInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * Returns responses for Mukurtu Collections routes.
  */
 class CollectionOrganizationController extends ControllerBase {
 
+  /**
+   * Gets the IDs of collections that the given user can use as subcollections.
+   *
+   * Currently this is
+   */
   protected function getEligibleChildCollections(AccountInterface $account) {
     $query = \Drupal::entityQuery('node')
       ->condition('type', 'collection')
@@ -21,27 +28,6 @@ class CollectionOrganizationController extends ControllerBase {
     $results = $query->execute();
 
     return $results;
-  }
-
-  protected function getCollectionOrganization(Collection $collection) {
-    $organization = [];
-    $visited = [];
-    $s = [[$collection, 0, 0, 0]];
-    while(!empty($s)) {
-      list($c, $parent_id, $weight, $level) = array_pop($s);
-      if (!isset($visited[$c->id()])) {
-        $visited[$c->id()] = TRUE;
-        $organization[] = ['title' => $c->getTitle(), 'id' => $c->id(), 'collection' => $c, 'parent' => $parent_id, 'weight' => $weight++, 'level' => $level];
-        $childWeight = 0;
-        $children = $c->getChildCollections();
-        $children = array_reverse($children);
-        foreach ($children as $child) {
-          $childInfo = [$child, $c->id(), $childWeight++, $level + 1];
-          array_push($s, $childInfo);
-        }
-      }
-    }
-    return $organization;
   }
 
   /**
@@ -62,17 +48,52 @@ class CollectionOrganizationController extends ControllerBase {
 
   public function access(NodeInterface $node, AccountInterface $account) {
     if ($node instanceof Collection && $node->access('update', $account)) {
-      if ($this->hasUpdateAccessToAllChildCollections($node, $account) && count($node->getChildCollectionIds()) > 0) {
+      if ($this->hasUpdateAccessToAllChildCollections($node, $account)) {
         return AccessResult::allowed();
       }
     }
     return AccessResult::forbidden();
   }
 
+
+  /**
+   * Autocomplete handler for collection organization form.
+   *
+   * Finds published collections user has edit access to.
+   * @see \Drupal\mukurtu_collection\Form\CollectionOrganizationForm
+   */
+  public function handleAutocomplete(Request $request) {
+    $results = [];
+    $input = $request->query->get('q');
+    if (strlen($input) > 0) {
+      $query = \Drupal::entityQuery('node')
+        ->condition('status', 1)
+        ->condition('title', '%' . $input . '%', 'LIKE')
+        ->condition('type', 'collection')
+        ->accessCheck(TRUE)
+        ->range(0, 10);
+      $nids = $query->execute();
+
+      if (!empty($nids)) {
+        $nodes = \Drupal\node\Entity\Node::loadMultiple($nids);
+        foreach ($nodes as $node) {
+          if ($node->access('update')) {
+            $results[] = [
+              'value' => $node->getTitle() . " ({$node->id()})",
+              'label' => $node->getTitle(),
+            ];
+          }
+        }
+      }
+    }
+
+    return new JsonResponse($results);
+  }
+
   public function build(NodeInterface $node) {
     $build = [];
     $eligibleCollections = [];//$this->getEligibleChildCollections($this->currentUser());
-    $build['form'] = \Drupal::formBuilder()->getForm('Drupal\mukurtu_collection\Form\CollectionOrganizationForm', $this->getCollectionOrganization($node), $eligibleCollections);
+    $build['form'] = \Drupal::formBuilder()->getForm('Drupal\mukurtu_collection\Form\CollectionOrganizationForm', $node, $eligibleCollections);
     return $build;
   }
 
--- modules/mukurtu_collection/src/Form/CollectionOrganizationForm.php
+++ modules/mukurtu_collection/src/Form/CollectionOrganizationForm.php
@@ -2,28 +2,35 @@
 
 namespace Drupal\mukurtu_collection\Form;
 
+use Drupal\Core\Ajax\AjaxResponse;
+use Drupal\Core\Ajax\ReplaceCommand;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
-use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\mukurtu_collection\Entity\Collection;
 use Symfony\Component\DependencyInjection\ContainerInterface;
+
 
 /**
  * Provides a Mukurtu Collections form.
  */
 class CollectionOrganizationForm extends FormBase {
 
+  /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
   protected $entityTypeManager;
 
-  public function __construct(EntityTypeManagerInterface $entity_type_manager)
-  {
+  public function __construct(EntityTypeManagerInterface $entity_type_manager) {
     $this->entityTypeManager = $entity_type_manager;
   }
 
   /**
    * {@inheritdoc}
    */
-  public static function create(ContainerInterface $container)
-  {
+  public static function create(ContainerInterface $container) {
     return new static(
       $container->get('entity_type.manager')
     );
@@ -37,10 +44,71 @@ class CollectionOrganizationForm extends FormBase {
   }
 
   /**
-   * {@inheritdoc}
+   * Build a collection organization array from a collection entity.
    */
-  public function buildForm(array $form, FormStateInterface $form_state, $collections = NULL, $available_collections = []) {
-    $form['collections'] = [
+  protected function getOrganizationFromCollection(Collection $collection) {
+    $organization = [];
+    $visited = [];
+    $s = [[$collection, 0, 0, 0]];
+    while(!empty($s)) {
+      list($c, $parent_id, $weight, $level) = array_pop($s);
+      if (!isset($visited[$c->id()])) {
+        $visited[$c->id()] = TRUE;
+        $organization[] = ['title' => $c->getTitle(), 'id' => $c->id(), 'collection' => $c, 'parent' => $parent_id, 'weight' => $weight++, 'level' => $level];
+        $childWeight = 0;
+        $children = $c->getChildCollections();
+        $children = array_reverse($children);
+        foreach ($children as $child) {
+          $childInfo = [$child, $c->id(), $childWeight++, $level + 1];
+          array_push($s, $childInfo);
+        }
+      }
+    }
+
+    return $organization;
+  }
+
+
+  /**
+   * Build the collection organization array from the form table values.
+   */
+  protected function getOrganizationFromValues($values) {
+    $organization = [];
+
+    if (empty($values)) {
+      return $organization;
+    }
+
+    $levels = [];
+    foreach ($values as $value) {
+      if ($collection = $this->entityTypeManager->getStorage('node')->load(reset($value['label']))) {
+
+        if ($value['parent'] == 0) {
+          $levels[$collection->id()] = 0;
+        } else {
+          $levels[$collection->id()] = $levels[(int) $value['parent']] + 1;
+        }
+
+        $organization[] = [
+          "title" => $collection->getTitle(),
+          "id" => $collection->id(),
+          "collection" => $collection,
+          "parent" => $value['parent'],
+          "weight" => $value['weight'],
+          "level" => $levels[$collection->id()],
+        ];
+      }
+    }
+
+    return $organization;
+  }
+
+
+  /**
+   * Build the collections table element based on a collections organization array.
+   */
+  protected function buildCollectionsTable($collectionsOrg) {
+    $element = [
       '#type' => 'table',
       '#caption' => $this->t('Collection Organization'),
       '#header' => [
@@ -48,6 +116,8 @@ class CollectionOrganizationForm extends FormBase {
         $this->t('Weight'),
         $this->t('Parent'),
       ],
+      '#prefix' => '<div id="collections-table">',
+      '#suffix' => '</div>',
       '#tableselect' => FALSE,
       '#tabledrag' => [
         [
@@ -66,11 +136,11 @@ class CollectionOrganizationForm extends FormBase {
       ]
     ];
 
-    foreach ($collections as $id => $info) {
+    foreach ($collectionsOrg as $id => $info) {
       $collection = $info['collection'];
-      $form['collections'][$id]['#weight'] = $info['weight'];
-      $form['collections'][$id]['#attributes']['class'][] = 'draggable';
-      $form['collections'][$id]['label'] = [
+      $element[$id]['#weight'] = $info['weight'];
+      $element[$id]['#attributes']['class'][] = 'draggable';
+      $element[$id]['label'] = [
         [
           '#theme' => 'indentation',
           '#size' => $info['level'],
@@ -86,7 +156,7 @@ class CollectionOrganizationForm extends FormBase {
         ],
       ];
 
-      $form['collections'][$id]['weight'] = [
+      $element[$id]['weight'] = [
         '#type' => 'weight',
         '#title' => $this->t('Weight for @title', ['@title' => $collection->getTitle()]),
         '#title_display' => 'invisible',
@@ -94,7 +164,7 @@ class CollectionOrganizationForm extends FormBase {
         '#attributes' => ['class' => ['field-weight']],
       ];
 
-      $form['collections'][$id]['parent'] = [
+      $element[$id]['parent'] = [
         '#type' => 'weight',
         '#title' => $this->t('Parent for @title', ['@title' => $collection->getTitle()]),
         '#title_display' => 'invisible',
@@ -102,6 +172,40 @@ class CollectionOrganizationForm extends FormBase {
         '#attributes' => ['class' => ['field-parent']],
       ];
     }
+
+    return $element;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state, Collection $collection = NULL, $available_collections = []) {
+    if (!$collection) {
+      return $form;
+    }
+
+    $collectionsValue = $form_state->getValue('collections') ?? NULL;
+    $collections_organization = $collectionsValue ? $this->getOrganizationFromValues($collectionsValue) : $this->getOrganizationFromCollection($collection);
+
+    $form['add_subcollection'] = [
+      '#type' => 'textfield',
+      '#title' => t('Add an existing collection as a sub-collection'),
+      '#description' => t('Start typing the title of an existing collection. The collection cannot be a sub-collection already. You must have edit access to the collection.'),
+      '#autocomplete_route_name' => 'mukurtu_collection.collection_organization_subcollection_autocomplete',
+      '#autocomplete_route_parameters' => ['node' => $collection->id()],
+    ];
+
+    $form['add'] = [
+      "#type" => "submit",
+      "#value" => "Add to Organization",
+      '#submit' => ['::addCollectionToOrganizationAjax'],
+      '#ajax' => [
+        'callback' => '::addCollectionToTableCallback',
+        'wrapper' => 'collections-table',
+      ],
+    ];
+
+    $form['collections'] = $this->buildCollectionsTable($collections_organization);
 
     $form['actions'] = [
       '#type' => 'actions',
@@ -113,6 +217,71 @@ class CollectionOrganizationForm extends FormBase {
 
     return $form;
   }
+
+  public function addCollectionToTableCallback(array &$form, FormStateInterface $form_state) {
+    $response = new AjaxResponse();
+    $response->addCommand(new ReplaceCommand('#collections-table', $form['collections']));
+    return $response;
+  }
+
+  protected function buildOrganizationFromTableCollections($tableCollections) {
+    $organization = [];
+    foreach ($tableCollections as $tableCollection) {
+      if ($id = $tableCollection['label'][2] ?? NULL) {
+        $collection = $this->entityTypeManager->getStorage('node')->load($id);
+
+        $organization[] = [
+          'title' => $collection->getTitle(),
+          'id' => $collection->id(),
+          'collection' => $collection,
+          'parent' => $tableCollection['parent'],
+          'weight' => $tableCollection['weight'],
+          'level' => 0,
+        ];
+      }
+    }
+    return $organization;
+  }
+
+  /**
+   * Check if a collection ID is in the table values.
+   */
+  protected function alreadyInCollectionsTable($collection_id, $table_values) {
+    foreach ($table_values as $value) {
+      if ($id = reset($value['label']) ?? NULL) {
+        if ($id == $collection_id) {
+          return TRUE;
+        }
+      }
+    }
+    return FALSE;
+  }
+
+  public function addCollectionToOrganizationAjax(array &$form, FormStateInterface $form_state) {
+    $newCollectionAutocompleteValue = $form_state->getValue('add_subcollection') ?? '';
+
+    $matches = [];
+    if (preg_match('/^(.*) \((\d+)\)$/', $newCollectionAutocompleteValue, $matches)) {
+      $id = $matches[2];
+      if ($collection = $this->entityTypeManager->getStorage('node')->load($id)) {
+        // Add the new collection to the values array.
+        $collectionValues = $form_state->getValue("collections");
+        if (!$this->alreadyInCollectionsTable($id, $collectionValues)) {
+          $collectionValues[] = [
+            'label' => [2 => $id],
+            'weight' => 0,
+            'parent' => 0,
+          ];
+          $form_state->setValue('collections', $collectionValues);
+
+          // Rebuild the collections table.
+          $form['collections'] = $this->buildCollectionsTable($this->getOrganizationFromValues($collectionValues));
+        }
+      }
+    }
+    $form_state->setRebuild(TRUE);
+  }
+
 
   /**
    * {@inheritdoc}
