4eae5fc0b9e4dcf53c6aa209d5c7f020970257de
Steve Taylor <steve_taylor@wsu.edu>
2023-11-01 11:19:05 -0700

    Sketching out export tests

---
+++ modules/mukurtu_export/tests/src/Kernel/CsvExportFieldTestBase.php
@@ -0,0 +1,80 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\Tests\mukurtu_export\Kernel;
+
+use Drupal\mukurtu_export\Entity\CsvExporter;
+use Drupal\mukurtu_export\EventSubscriber\CsvEntityFieldExportEventSubscriber;
+use Drupal\mukurtu_protocol\Entity\Community;
+use Drupal\mukurtu_protocol\Entity\Protocol;
+use Drupal\Tests\mukurtu_protocol\Kernel\ProtocolAwareEntityTestBase;
+
+class CsvExportFieldTestBase extends ProtocolAwareEntityTestBase {
+
+  /**
+   * @var \Drupal\mukurtu_protocol\Entity\Community
+   */
+  protected $community;
+
+  /**
+   * @var \Drupal\mukurtu_protocol\Entity\Protocol
+   */
+  protected $protocol;
+
+  protected $context;
+
+  /**
+   * @var \Drupal\mukurtu_export\EventSubscriber\CsvEntityFieldExportEventSubscriber
+   */
+  protected $fieldExporter;
+
+  /**
+   * @var \Drupal\mukurtu_export\Event\EntityFieldExportEvent
+   */
+  protected $event;
+
+  /**
+   * @var \Drupal\mukurtu_export\Entity\CsvExporter
+   */
+  protected $export_config;
+
+  /**
+   * Modules to enable.
+   *
+   * @var array
+   */
+  protected static $modules = [
+    'mukurtu_export',
+  ];
+
+  protected function setUp(): void {
+    parent::setUp();
+    $community = Community::create([
+      'name' => 'Community',
+    ]);
+    $community->save();
+    $this->community = $community;
+    $this->community->addMember($this->currentUser);
+
+    $protocol = Protocol::create([
+      'name' => "Protocol",
+      'field_communities' => [$this->community->id()],
+      'field_access_mode' => 'open',
+    ]);
+    $protocol->save();
+    $protocol->addMember($this->currentUser, ['protocol_steward']);
+    $this->protocol = $protocol;
+
+    $this->export_config = new CsvExporter([], 'node');
+
+    $this->context = [
+      'results' => [
+        'config_id' => $this->export_config->id(),
+      ]
+    ];
+
+    $this->fieldExporter = new CsvEntityFieldExportEventSubscriber(\Drupal::messenger(), \Drupal::entityTypeManager());
+  }
+
+}
---
+++ modules/mukurtu_export/tests/src/Kernel/CsvExportTextFieldTest.php
@@ -0,0 +1,64 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\Tests\mukurtu_export\Kernel;
+
+use Drupal\node\Entity\Node;
+use Drupal\mukurtu_export\Event\EntityFieldExportEvent;
+use Drupal\field\Entity\FieldStorageConfig;
+use Drupal\field\Entity\FieldConfig;
+
+class CsvExportTextFieldTest extends CsvExportFieldTestBase {
+
+  protected $node;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp(): void {
+    parent::setUp();
+
+    $field_storage = FieldStorageConfig::create([
+      'field_name' => 'field_text_multiple',
+      'entity_type' => 'node',
+      'type' => 'string',
+      'cardinality' => -1,
+    ]);
+    $field_storage->save();
+
+    FieldConfig::create([
+      'field_name' => 'field_text_multiple',
+      'entity_type' => 'node',
+      'bundle' => 'protocol_aware_content',
+      'label' => 'Text',
+    ])->save();
+
+    $node = Node::create([
+      'title' => 'Testing Export',
+      'type' => 'protocol_aware_content',
+      'field_text_multiple' => ['String 1', 'String 2'],
+      'status' => TRUE,
+      'uid' => $this->currentUser->id(),
+    ]);
+    $node->setSharingSetting('any');
+    $node->setProtocols([$this->protocol]);
+    $node->save();
+    $this->node = $node;
+    $this->event = new EntityFieldExportEvent('csv', $this->node, 'title', $this->context);
+  }
+
+  public function testSingleTextFieldExport() {
+    $this->fieldExporter->exportField($this->event);
+    $this->assertEquals($this->node->getTitle(), $this->event->getValue()[0]);
+  }
+
+  public function testMultipleTextFieldExport() {
+    $event = new EntityFieldExportEvent('csv', $this->node, 'field_text_multiple', $this->context);
+    $this->fieldExporter->exportField($event);
+    $this->assertCount(2, $event->getValue());
+    $this->assertEquals('String 1', $event->getValue()[0]);
+    $this->assertEquals('String 2', $event->getValue()[1]);
+  }
+
+}
