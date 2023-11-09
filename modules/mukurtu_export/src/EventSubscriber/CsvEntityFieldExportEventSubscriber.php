10daab50ab170e71c8f5aa77113787fdcb6badb8
Steve Taylor <steve_taylor@wsu.edu>
2023-11-09 10:53:15 -0800

    Catch the exception thrown when field not found

--- modules/mukurtu_export/src/EventSubscriber/CsvEntityFieldExportEventSubscriber.php
+++ modules/mukurtu_export/src/EventSubscriber/CsvEntityFieldExportEventSubscriber.php
@@ -3,18 +3,32 @@
 namespace Drupal\mukurtu_export\EventSubscriber;
 
 use Drupal\Core\Entity\EntityInterface;
-use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-use Drupal\mukurtu_export\Event\EntityFieldExportEvent;
-use Drupal\mukurtu_export\Entity\CsvExporter;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Messenger\MessengerInterface;
+use Drupal\mukurtu_export\Entity\CsvExporter;
+use Drupal\mukurtu_export\Event\EntityFieldExportEvent;
+use InvalidArgumentException;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
-class CsvEntityFieldExportEventSubscriber implements EventSubscriberInterface
-{
+class CsvEntityFieldExportEventSubscriber implements EventSubscriberInterface {
 
+  /**
+   * The messenger.
+   *
+   * @var \Drupal\Core\Messenger\MessengerInterface
+   */
   protected $messenger;
+
+  /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
   protected $entityTypeManager;
 
+  /**
+   * {@inheritDoc}
+   */
   public function __construct(MessengerInterface $messenger, EntityTypeManagerInterface $entity_type_manager) {
     $this->messenger = $messenger;
     $this->entityTypeManager = $entity_type_manager;
@@ -23,15 +37,21 @@ class CsvEntityFieldExportEventSubscriber implements EventSubscriberInterface
   /**
    * {@inheritdoc}
    */
-  public static function getSubscribedEvents()
-  {
+  public static function getSubscribedEvents() {
     return [
       EntityFieldExportEvent::EVENT_NAME => ['exportField', 100],
     ];
   }
 
-  public function exportField(EntityFieldExportEvent $event)
-  {
+  /**
+   * Handler for the EntityFieldExportEvent.
+   *
+   * @param \Drupal\mukurtu_export\Event\EntityFieldExportEvent $event
+   *   The event.
+   *
+   * @return mixed
+   */
+  public function exportField(EntityFieldExportEvent $event) {
     if ($event->exporter_id != 'csv') {
       return;
     }
@@ -40,7 +60,11 @@ class CsvEntityFieldExportEventSubscriber implements EventSubscriberInterface
     /** @var \Drupal\mukurtu_export\Entity\CsvExporter $config */
     $config = $this->entityTypeManager->getStorage('csv_exporter')->load($event->context['results']['config_id']);
 
-    $field = $entity->get($field_name);
+    try {
+      $field = $entity->get($field_name);
+    } catch (InvalidArgumentException $e) {
+      return NULL;
+    }
     $fieldType = $field->getFieldDefinition()->getType() ?? NULL;
 
     if ($fieldType == 'file') {
@@ -68,12 +92,54 @@ class CsvEntityFieldExportEventSubscriber implements EventSubscriberInterface
     $event->setValue($exportValue);
   }
 
+  /**
+   * Retrieves the UUID for a given entity or a concatenated type:id string if not found.
+   *
+   * This method attempts to load an entity of the specified type and ID and return
+   * its UUID. If the entity cannot be loaded, it returns a string combining the
+   * entity type and ID.
+   *
+   * @param string $entity_type_id
+   *   The entity type ID for which to retrieve the UUID.
+   * @param mixed $id
+   *   The ID of the entity for which to retrieve the UUID.
+   *
+   * @return string
+   *   The UUID of the entity if found, or a string in the format "{entity_type_id}:{id}"
+   *   if the entity is not found.
+   *
+   * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
+   *   If the storage handler class for the entity type does not exist.
+   * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
+   *   If the entity type does not exist.
+   *
+   * @protected
+   */
   protected function getUUID($entity_type_id, $id) {
-    if($entity = $this->entityTypeManager->getStorage($entity_type_id)->load($id)) {
+    if ($entity = $this->entityTypeManager->getStorage($entity_type_id)->load($id)) {
       return $entity->uuid();
     }
     return "{$entity_type_id}:{$id}";
   }
+
+  /**
+   * Exports the entity reference field values for a given CSV export configuration.
+   *
+   * This method processes an entity reference field and exports the field values
+   * based on the configuration provided. It supports exporting entity IDs, UUIDs,
+   * usernames for user entities, and term names for taxonomy terms, depending on
+   * the exporter settings. The resulting export values are then set in the event object.
+   *
+   * @param EntityFieldExportEvent $event
+   *   The entity field export event containing context and environment for the export.
+   * @param \Drupal\Core\Field\FieldItemListInterface $field
+   *   The field items list being exported, which contains the entity reference data.
+   * @param CsvExporter $config
+   *   The configuration object that provides settings for the export, such as the
+   *   desired format of the entity reference export (e.g., ID, UUID, username).
+   *
+   * @protected
+   */
   protected function exportEntityReference(EntityFieldExportEvent $event, $field, CsvExporter $config) {
     $export = [];
     $target_type = $field->getFieldDefinition()->getSettings()['target_type'] ?? NULL;
@@ -117,6 +183,26 @@ class CsvEntityFieldExportEventSubscriber implements EventSubscriberInterface
     $event->setValue($export);
   }
 
+  /**
+   * Exports the cultural protocol field values according to the provided configuration.
+   *
+   * Processes the cultural protocol field from the event's context and formats it based on
+   * the CSV exporter settings. It allows for the protocol identifiers to be exported as UUIDs
+   * or as their original IDs, formatted and concatenated into a string representation.
+   *
+   * @param EntityFieldExportEvent $event
+   *   The event object containing the context and settings for the current export operation.
+   * @param \Drupal\Core\Field\FieldItemListInterface $field
+   *   The field items list being exported, which contains the cultural protocol data.
+   * @param CsvExporter $config
+   *   The configuration object that provides settings for the export, such as the
+   *   desired format for the ID field.
+   *
+   * @throws \Exception
+   *   If any errors occur during the retrieval of UUIDs for the protocols.
+   *
+   * @protected
+   */
   protected function exportCulturalProtocol(EntityFieldExportEvent $event, $field, CsvExporter $config) {
     $export = [];
     $id_format = $config->getIdFieldSetting();
@@ -133,9 +219,26 @@ class CsvEntityFieldExportEventSubscriber implements EventSubscriberInterface
     $event->setValue($export);
   }
 
-
-  protected function exportFile(EntityFieldExportEvent $event, $field, CsvExporter $config)
-  {
+  /**
+   * Exports the file field values based on the specified export configuration.
+   *
+   * This method handles the export of file fields by either packaging the file and exporting the path,
+   * exporting the file entity, or by providing the file identifier (ID or UUID). The chosen method
+   * depends on the configuration specified in the CsvExporter.
+   *
+   * @param EntityFieldExportEvent $event
+   *   The export event object which provides the context and environment for the current export operation.
+   * @param \Drupal\Core\Field\FieldItemListInterface $field
+   *   The field items list containing the file data to be exported.
+   * @param CsvExporter $config
+   *   The configuration object that dictates how file fields should be exported.
+   *
+   * @throws \Drupal\Core\Entity\EntityStorageException
+   *   Thrown if there is an issue loading or packaging the file entity.
+   *
+   * @protected
+   */
+  protected function exportFile(EntityFieldExportEvent $event, $field, CsvExporter $config) {
     $setting = $config->getFileFieldSetting();
     $export = [];
 
@@ -163,6 +266,27 @@ class CsvEntityFieldExportEventSubscriber implements EventSubscriberInterface
     $event->setValue($export);
   }
 
+  /**
+   * Exports the image field values based on the specified export configuration.
+   *
+   * This method exports image field data in various formats based on the exporter settings.
+   * It supports packaging the image file with its path, exporting the whole file entity,
+   * or exporting the file identifier (ID or UUID).
+   *
+   * @param EntityFieldExportEvent $event
+   *   The export event object which provides the context and the necessary environment
+   *   for the current export operation.
+   * @param \Drupal\Core\Field\FieldItemListInterface $field
+   *   The field items list containing image data to be exported.
+   * @param CsvExporter $config
+   *   The configuration object that dictates how image fields should be exported,
+   *   including the format of the image identifier and whether to include binary data.
+   *
+   * @throws \Drupal\Core\Entity\EntityStorageException
+   *   Thrown if there is an issue loading or packaging the image file entity.
+   *
+   * @protected
+   */
   protected function exportImage(EntityFieldExportEvent $event, $field, CsvExporter $config) {
     $setting = $config->getImageFieldSetting();
     $export = [];
@@ -191,6 +315,30 @@ class CsvEntityFieldExportEventSubscriber implements EventSubscriberInterface
     $event->setValue($export);
   }
 
+  /**
+   * Loads an entity by its ID and triggers its export if found.
+   *
+   * This method attempts to load an entity of the specified type and ID. If the entity
+   * is successfully loaded, it is passed to the export event to handle additional export
+   * logic and is then returned. If no entity is found, NULL is returned.
+   *
+   * @param EntityFieldExportEvent $event
+   *   The export event object which provides context for the export operation.
+   * @param string $entity_type_id
+   *   The type of entity to load, such as 'node' or 'user'.
+   * @param mixed $id
+   *   The unique identifier for the entity to load.
+   *
+   * @return \Drupal\Core\Entity\EntityInterface|null
+   *   The entity object if found, or NULL otherwise.
+   *
+   * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
+   *   Thrown if the storage handler class for the entity type does not exist.
+   * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
+   *   Thrown if the entity type does not exist.
+   *
+   * @protected
+   */
   protected function exportEntityById(EntityFieldExportEvent $event, $entity_type_id, $id): EntityInterface|null {
     if ($entity = $this->entityTypeManager->getStorage($entity_type_id)->load($id)) {
       $event->exportAdditionalEntity($entity);
@@ -199,6 +347,27 @@ class CsvEntityFieldExportEventSubscriber implements EventSubscriberInterface
     return NULL;
   }
 
+  /**
+   * Prepares a file for export and returns the packaged file path.
+   *
+   * This method loads a file entity based on the provided file ID (fid) and then
+   * prepares it for export by packaging it into a predefined file structure.
+   * If the file is successfully loaded and packaged, the path to the packaged file
+   * is returned. If no file is found, NULL is returned.
+   *
+   * @param EntityFieldExportEvent $event
+   *   The export event object which provides context for the export operation.
+   * @param int|string $fid
+   *   The unique identifier for the file to be packaged.
+   *
+   * @return string|null
+   *   The path to the packaged file if the file entity is found, or NULL otherwise.
+   *
+   * @throws \Drupal\Core\Entity\EntityStorageException
+   *   Thrown if there is an issue loading the file entity.
+   *
+   * @protected
+   */
   protected function packageFile(EntityFieldExportEvent $event, $fid): string|null {
     if ($file = $this->entityTypeManager->getStorage('file')->load($fid)) {
       $packagedFilePath = sprintf("%s/%s/%s", "files", $fid, $file->getFilename());
