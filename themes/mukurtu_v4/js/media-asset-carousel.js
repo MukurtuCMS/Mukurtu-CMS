acd830684debc8ff8a39cc5d71859178625bff09
Pauline Judge <pauline.judge@lullabot.com>
2024-04-25 15:26:26 -0400

    update display modes with user friendly names

--- themes/mukurtu_v4/js/media-asset-carousel.js
+++ themes/mukurtu_v4/js/media-asset-carousel.js
@@ -17,21 +17,21 @@
       attach(context) {
         once("mediaAssets", ".field--name-field-media-assets", context).forEach(init);
 
-        // if (Drupal.behaviors.mediaAssetCarousel.mediaAssetSlider == undefined) {
-        //  console.log("media asset slider undefined");
+        if (Drupal.behaviors.mediaAssetCarousel.mediaAssetSlider == undefined) {
+         console.log("media asset slider undefined");
 
-        //   Drupal.behaviors.mediaAssetCarousel.mediaAssetSlider = new Splide('.splide.media-carousel', {
-        //     perPage: 3,
-        //     isNavigation: true,
-        //     pagination: false,
-        //     updateOnMove: false,
-        //     trimSpace: true,
-        //     autoWidth: true,
-        //     autoHeight: true,
-        //     gap: '16px',
-        //     start: 0,
-        //   }).mount();
-        // }
+          Drupal.behaviors.mediaAssetCarousel.mediaAssetSlider = new Splide('.splide.media-carousel', {
+            perPage: 3,
+            isNavigation: true,
+            pagination: false,
+            updateOnMove: false,
+            trimSpace: true,
+            autoWidth: true,
+            autoHeight: true,
+            gap: '16px',
+            start: 0,
+          }).mount();
+        }
       }
     };
   })(Drupal, once);
