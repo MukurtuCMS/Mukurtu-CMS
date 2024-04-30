a513c4641585ff4fca7c0e33734a26234bae70fe
Pauline Judge <pauline.judge@lullabot.com>
2024-04-30 14:53:04 -0400

    preprocess for rendering media assets field twice on DH nodes with two different display modes for the synced carousel

--- themes/mukurtu_v4/js/media-asset-carousel.js
+++ themes/mukurtu_v4/js/media-asset-carousel.js
@@ -9,49 +9,51 @@
      * Initialize the carousel.
      */
     function init(el) {
-      // main.sync( thumbnails );
-      // main.mount();
-      // thumbnails.mount();
+      main = new Splide( '.splide.media-carousel', {
+        type      : 'fade',
+        rewind    : true,
+        pagination: false,
+        arrows    : false,
+      } );
+    
+      thumbnails = new Splide( '.splide.thumbnail-carousel', {
+        fixedWidth  : 100,
+        fixedHeight : 60,
+        gap         : 10,
+        rewind      : true,
+        pagination  : false,
+        isNavigation: true,
+        breakpoints : {
+          600: {
+            fixedWidth : 60,
+            fixedHeight: 44,
+          },
+        },
+      } );
+      
+      main.sync( thumbnails );
+      main.mount();
+      thumbnails.mount();
     }
   
     Drupal.behaviors.mediaAssetCarousel = {
       attach(context) {
-        once("mediaAssets", ".field--name-field-media-assets", context).forEach(init);
+        once("mediaAssets", ".media-carousels", context).forEach(init);
 
-        // main = new Splide( '#main-carousel', {
-        //   type      : 'fade',
-        //   rewind    : true,
-        //   pagination: false,
-        //   arrows    : false,
-        // } );
-      
-        // thumbnails = new Splide( '.splide.media-carousel', {
-        //   fixedWidth  : 100,
-        //   fixedHeight : 60,
-        //   gap         : 10,
-        //   rewind      : true,
-        //   pagination  : false,
-        //   isNavigation: true,
-        //   breakpoints : {
-        //     600: {
-        //       fixedWidth : 60,
-        //       fixedHeight: 44,
-        //     },
-        //   },
-        // } );
+       
 
 
-          Drupal.behaviors.mediaAssetCarousel.mediaAssetSlider = new Splide('.splide.media-carousel', {
-            perPage: 3,
-            isNavigation: true,
-            pagination: false,
-            updateOnMove: false,
-            trimSpace: true,
-            autoWidth: true,
-            autoHeight: true,
-            gap: '16px',
-            start: 0,
-          }).mount();
+          // Drupal.behaviors.mediaAssetCarousel.mediaAssetSlider = new Splide('.splide.media-carousel', {
+          //   perPage: 3,
+          //   isNavigation: true,
+          //   pagination: false,
+          //   updateOnMove: false,
+          //   trimSpace: true,
+          //   autoWidth: true,
+          //   autoHeight: true,
+          //   gap: '16px',
+          //   start: 0,
+          // }).mount();
 
       }
     };
