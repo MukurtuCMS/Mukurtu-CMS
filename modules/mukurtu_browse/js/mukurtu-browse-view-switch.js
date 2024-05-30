abf5ca51453cec506e0b01efbefb1db2a4dba574
Pauline Judge <pauline.judge@lullabot.com>
2024-05-30 15:36:37 -0400

    adds local storage for maintaining browse display

--- modules/mukurtu_browse/js/mukurtu-browse-view-switch.js
+++ modules/mukurtu_browse/js/mukurtu-browse-view-switch.js
@@ -3,63 +3,111 @@
 
 ((Drupal, once) => {
   "use strict";
-   let gridLink, listLink, mapLink, mapContent, listContent, gridContent;
-   
-   function switchToMap() {
-    mapLink.classList.add("active-toggle");
-    listLink.classList.remove("active-toggle");
-    gridLink.classList.remove("active-toggle");
+   let gridLink, listLink, mapLink, mapContent, listContent, gridContent, browseLinks, button;
 
-    mapContent.hidden = false;
-    listContent.hidden = true;
-    gridContent.hidden = true;
+  function listCheck() {
+
+    if (localStorage.getItem("browseMode") == "list") {
+      return true;
+    } else {
+      return false;
+    }
   }
 
-  function switchToList() {
-    listLink.classList.add("active-toggle");
-    gridLink.classList.remove("active-toggle");
-    mapLink.classList.remove("active-toggle");
-
-    listContent.hidden = false;
-    mapContent.hidden = true;
-    gridContent.hidden = true;
+  function gridCheck() {
+    if (localStorage.getItem("browseMode") == "grid") {
+      return true;
+    } else {
+      return false;
+    }
   }
 
-  function switchToGrid() {
-    gridLink.classList.add("active-toggle");
-    listLink.classList.remove("active-toggle");
-    mapLink.classList.remove("active-toggle");
+  function mapCheck() {
+    if (localStorage.getItem("browseMode") == "map") {
+      return true;
+    } else {
+      return false;
+    }
+  }
 
-    gridContent.hidden = false;
-    mapContent.hidden = true;
-    listContent.hidden = true;
+  function toggleBrowseMode(e) {
+
+    button = e.target.getAttribute('id');
+
+    // First set the cookie. 
+    if (button == "mukurtu-browse-list") {
+      localStorage.setItem("browseMode", "list");
+    } else if (button == "mukurtu-browse-grid") {
+      localStorage.setItem("browseMode", "grid");
+    } else if (button == "mukurtu-browse-map"){
+      localStorage.setItem("browseMode", "map");
+    }
+
+    // Then perform class toggling/content visibility 
+    // toggling based on which cookie set.
+    if (listCheck()) {
+      listLink.classList.add("active-toggle");
+      gridLink.classList.remove("active-toggle");
+      mapLink.classList.remove("active-toggle");
+
+      listContent.hidden = false;
+      mapContent.hidden = true;
+      gridContent.hidden = true;
+    }
+
+    if (gridCheck()) {
+      gridLink.classList.add("active-toggle");
+      listLink.classList.remove("active-toggle");
+      mapLink.classList.remove("active-toggle");
+
+      gridContent.hidden = false;
+      mapContent.hidden = true;
+      listContent.hidden = true;
+    }
+
+    if (mapCheck()) {
+      mapLink.classList.add("active-toggle");
+      listLink.classList.remove("active-toggle");
+      gridLink.classList.remove("active-toggle");
+
+      mapContent.hidden = false;
+      listContent.hidden = true;
+      gridContent.hidden = true;
+    }
   }
 
 
   function init() {
     listContent = document.querySelector(".list");
-    listContent.hidden = false;
     mapContent = document.querySelector(".map");
-    mapContent.hidden = true;
     gridContent = document.querySelector(".grid");
-    gridContent.hidden = true;
+    browseLinks = document.querySelector(".browse-links");
 
     gridLink = document.getElementById("mukurtu-browse-grid");
     listLink = document.getElementById("mukurtu-browse-list");
     mapLink = document.getElementById("mukurtu-browse-map");
-    mapContent = document.querySelector(".browse-content.map");
-    listContent = document.querySelector(".browse-content.list");
-    gridContent = document.querySelector(".browse-content.grid");
 
-    listLink.addEventListener("click", switchToList);
-    gridLink.addEventListener("click", switchToGrid);
-    mapLink.addEventListener("click", switchToMap);
+    browseLinks.addEventListener("click", toggleBrowseMode);
+
+    let cookie = localStorage.getItem("browseMode");
+
+    // If cookie exists already, read it and 
+    // simulate a click on the corresponding button.
+    if (cookie) {
+      button = `mukurtu-browse-${cookie}`;
+      const event = new MouseEvent("click", {
+        view: window,
+        bubbles: true,
+        cancelable: true,
+      });
+      const cb = document.getElementById(button);
+      cb.dispatchEvent(event);
+    }
   }
-
 
   Drupal.behaviors.mukurtuBrowseSwitch = {
     attach(context) {
-      once("browseLinks", ".browse-links", context).forEach(init);
+      once("browseContainer", ".browse-container", context).forEach(init);
     },
   };
   
