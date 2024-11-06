8351a7c2e5bdeea582edff70ac9f4854d28e6b46
quicksketch <nate@quicksketch.org>
2024-11-06 11:06:17 -0800

    615: Set up Tugboat for Mukurtu.

---
+++ .tugboat/settings.tugboat.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * @file settings.local.php
+ *
+ * Local environment overrides and settings.
+ */
+
+declare(strict_types=1);
+
+// Set database settings for Tugboat.
+$databases['default']['default'] = [
+  'database' => 'tugboat',
+  'username' => 'tugboat',
+  'password' => 'tugboat',
+  'prefix' => '',
+  'host' => 'mariadb',
+  'port' => '3306',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+  'driver' => 'mysql',
+];
+
+// Use the TUGBOAT_REPO_ID to generate a hash salt for Tugboat sites.
+$settings['hash_salt'] = hash('sha256', getenv('TUGBOAT_REPO_ID'));
+
+// Set trusted hosts to disallow pointing other domains at the previews.
+$settings['trusted_host_patterns'] = [
+  '\.tugboatqa\.com$',
+];
+
+// Prevent Drupal from setting read-only permissions on sites/default.
+$settings['skip_permissions_hardening'] = TRUE;
