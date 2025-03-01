c484e778a4829c831443ae25e75477e9ae94ac1d
quicksketch <nate@quicksketch.org>
2025-02-28 16:35:33 -0800

    110: Upgrade Leaflet to version 10.2, removing custom widget and formatter.

--- modules/mukurtu_core/js/mukurtu-leaflet-widget.js
+++ modules/mukurtu_core/js/mukurtu-leaflet-widget.js
@@ -1,29 +1,8 @@
-(function ($, Drupal, drupalSettings) {
-  Drupal.behaviors.mukurtu_core_leaflet_widget = {
-    attach: function (context, settings) {
-      $(document).ready(function () {
-        var refresh = function () {
-
-          $.each(settings.leaflet_widget, function (map_id, widgetSettings) {
-            $('#' + map_id, context).each(function () {
-              let map = $(this);
-              let lMap = drupalSettings.leaflet[map_id].lMap;
-
-              // Refreshes map data to load with correct size and bounds.
-              lMap.invalidateSize();
-              map.data('leaflet_widget', new Drupal.leaflet_widget(map, lMap, widgetSettings));
-            });
-
-          });
-        };
-
-        // Bind refresh function when changing horizontal tab.
-        $('.horizontal-tabs-list').find('.horizontal-tab-button').each(function (key, tab) {
-          $(tab).find('a').bind('click', refresh);
-        });
-      });
-    }
-  };
+(function ($, Drupal) {
+  // These two prototype methods are overridden. Save the original methods so
+  // that they can be called by the overridden versions.
+  const original_set_leaflet_widget_map = Drupal.Leaflet_Widget.prototype.set_leaflet_widget_map;
+  const original_add_layer_listeners = Drupal.Leaflet_Widget.prototype.add_layer_listeners;
 
   /**
    * Save location description.
@@ -31,45 +10,18 @@
   Drupal.mukurtuSetLocationDescription = function (containerId, popupId) {
     Drupal.Leaflet[containerId].lMap._layers[popupId].feature.properties['location_description'] = $("#location-popup-" + popupId)[0].value;
     Drupal.Leaflet[containerId].lMap._layers[popupId].closePopup();
-  }
-
+  };
 
   /**
    * Set the leaflet map object.
    */
-  Drupal.leaflet_widget.prototype.set_leaflet_map = function (map) {
+  Drupal.Leaflet_Widget.prototype.set_leaflet_widget_map = function (map) {
+    // Call the original set_leaflet_widget_map():
+    original_set_leaflet_widget_map(map);
+
+    // Mukurtu additions:
     if (map !== undefined) {
       this.map = map;
-      map.addLayer(this.drawnItems);
-
-      if (this.settings.scrollZoomEnabled) {
-        map.on('focus', function () {
-          map.scrollWheelZoom.enable();
-        });
-        map.on('blur', function () {
-          map.scrollWheelZoom.disable();
-        });
-      }
-
-      // Adjust toolbar to show defaultMarker or circleMarker.
-      this.settings.toolbarSettings.drawMarker = false;
-      this.settings.toolbarSettings.drawCircleMarker = false;
-      if (this.settings.toolbarSettings.marker === "defaultMarker") {
-        this.settings.toolbarSettings.drawMarker = 1;
-      } else if (this.settings.toolbarSettings.marker === "circleMarker") {
-        this.settings.toolbarSettings.drawCircleMarker = 1;
-      }
-      map.pm.addControls(this.settings.toolbarSettings);
-
-      map.on('pm:create', function (event) {
-        let layer = event.layer;
-        this.drawnItems.addLayer(layer);
-        layer.pm.enable({ allowSelfIntersection: false });
-        this.update_text();
-        // Listen to changes on the new layer
-        this.add_layer_listeners(layer);
-      }, this);
-      this.update_map();
 
       // Mukurtu Location Description pop-up.
       map.on('popupopen', function (event) {
@@ -82,12 +34,16 @@
         $('#location-popup-' + event.popup._source._leaflet_id).val(locationDescription);
       }, this);
     }
+
   };
 
   /**
    * Add/Set Listeners to the Drawn Map Layers.
    */
-  Drupal.leaflet_widget.prototype.add_layer_listeners = function (layer) {
+  Drupal.Leaflet_Widget.prototype.add_layer_listeners = function (layer) {
+    // Call the original add_layer_listeners():
+    original_add_layer_listeners(layer);
+
     // Mukurtu Location Description.
     const containerId = $(layer._map._container).attr('id');
     const popupId = "location-popup-" + layer._leaflet_id;
@@ -96,34 +52,6 @@
       this.update_text();
     }, this);
 
-    // Listen to changes on the layer.
-    layer.on('pm:edit', function (event) {
-      this.update_text();
-    }, this);
-
-    // Listen to changes on the layer.
-    layer.on('pm:update', function (event) {
-      this.update_text();
-    }, this);
-
-    // Listen to drag events on the layer.
-    layer.on('pm:dragend', function (event) {
-      this.update_text();
-    }, this);
-
-    // Listen to cut events on the layer.
-    layer.on('pm:cut', function (event) {
-      this.drawnItems.removeLayer(event.originalLayer);
-      this.drawnItems.addLayer(event.layer);
-      this.update_text();
-    }, this);
-
-    // Listen to remove events on the layer.
-    layer.on('pm:remove', function (event) {
-      this.drawnItems.removeLayer(event.layer);
-      this.update_text();
-    }, this);
-
   };
 
-})(jQuery, Drupal, drupalSettings);
+})(jQuery, Drupal);
