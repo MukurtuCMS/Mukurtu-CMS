0754db1dbb030f6d8b3981b7f581a69b3de8932a
Chris DeLuca <637174+bronzehedwick@users.noreply.github.com>
2025-03-21 19:26:42 -0400

    Migrate to stylelint (#739)
    * Improve Gulp implementation
    
    Upgrade Gulp packages, or migrate to maintained, non-gulp versions.
    Remove unused or unneeded packages. Migrate from the unmaintained
    SASS Lint to stylelint.
    
    * Fix stylelint errors
    
    * Remove deprecated SASS syntax
    
    * Fix Sass mixed declaration deprecations
    
    See https://sass-lang.com/documentation/breaking-changes/mixed-decls/
    
    * Rebuild after merge

--- themes/mukurtu_v4/gulpfile.js
+++ themes/mukurtu_v4/gulpfile.js
@@ -1,20 +1,84 @@
 "use strict";
 
-const gulp = require("gulp"),
-  livereload = require("gulp-livereload"),
-  gulpIf = require("gulp-if"),
-  eslint = require("gulp-eslint"),
-  sassLint = require("gulp-sass-lint"),
-  autoprefixer = require("gulp-autoprefixer"),
-  sourcemaps = require("gulp-sourcemaps"),
-  imagemin = require("gulp-imagemin"),
-  pngquant = require("imagemin-pngquant");
+const { series, parallel, watch, src, dest } = require("gulp");
+const stylelint = require("stylelint");
+const autoprefixer = require("gulp-autoprefixer");
+const sourcemaps = require("gulp-sourcemaps");
+const dartSass = require("sass");
+const gulpSass = require("gulp-sass");
+const { ESLint } = require("eslint");
+const imagemin = require("gulp-imagemin");
+const pngquant = require("imagemin-pngquant");
 
-const sassGlob = require("gulp-sass-glob");
+const sassCompiler = gulpSass(dartSass);
 
-gulp.task("imagemin", function () {
-  return gulp
-    .src("./src/images/*")
+async function lintStyles(cb) {
+  return stylelint.lint({
+    files: [
+      "./components/**/*.scss",
+      "./css/*.css"
+    ],
+    formatter: 'string',
+    console: true
+  })
+    .then((data) => {
+      process.stdout.write(data.report);
+      cb();
+    })
+    .catch((err) => {
+      console.error(err.stack);
+      cb(err);
+    });
+}
+
+function buildStyles() {
+  return src("./components/**/*.scss")
+    .pipe(sourcemaps.init())
+    .pipe(sassCompiler({ style: "compressed" }).on("error", sassCompiler.logError))
+    .pipe(autoprefixer("last 2 versions"))
+    .pipe(sourcemaps.write("."))
+    .pipe(dest("./css"));
+}
+
+async function lintAndFixJS(eslint, filePaths) {
+  const results = await eslint.lintFiles(filePaths);
+  await ESLint.outputFixes(results);
+  return results;
+}
+
+function outputJSLintingResults(results) {
+  const problems = results.reduce(
+    (acc, result) => acc + result.errorCount + result.warningCount,
+    0,
+  );
+  if (problems > 0) {
+    for (const result of results) {
+      console.log(result.filePath);
+      for (const message of result.messages) {
+        console.log(`\t${message.ruleId}: ${message.message} on line ${message.line}`);
+      }
+      console.log("\n");
+    }
+  }
+  return results;
+}
+
+async function lintScripts() {
+  const results = await lintAndFixJS(
+    new ESLint({
+      fix: true,
+      errorOnUnmatchedPattern: false,
+    }),
+    [
+      "./components/**/*.js",
+      "./js/**/*.js"
+    ]
+  );
+  return outputJSLintingResults(results);
+}
+
+function minifyImages() {
+  return src("./src/images/**/*")
     .pipe(
       imagemin({
         progressive: true,
@@ -22,149 +86,27 @@ gulp.task("imagemin", function () {
         use: [pngquant()],
       })
     )
-    .pipe(gulp.dest("./images"));
-});
-
-gulp.task("sass", function (done) {
-  gulp
-    .src("./components/**/*.scss")
-    .pipe(sourcemaps.init())
-    .pipe(sassGlob())
-    // Lint first
-    .pipe(sassLint())
-    .pipe(sassLint.format())
-    .pipe(sassLint.failOnError())
-    .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
-    .pipe(autoprefixer("last 2 version"))
-    .pipe(sourcemaps.write("./"))
-    .pipe(gulp.dest("./css"));
-    done();
-});
-
-function isFixed(file) {
-  // Has ESLint fixed the file contents?
-  return file.eslint != null && file.eslint.fixed;
+    .pipe(dest("./images"));
 }
 
-gulp.task("eslint", function () {
-  gulp
-    .src("./components/**/*.js")
-    .pipe(
-      eslint({
-        extends: "eslint:recommended",
-        env: {
-          browser: true,
-        },
-        globals: {
-          Drupal: true,
-          drupalSettings: true,
-          drupalTranslations: true,
-          domready: true,
-          jQuery: true,
-          _: true,
-          matchMedia: true,
-          Backbone: true,
-          Modernizr: true,
-          CKEDITOR: true,
-        },
-        rules: {
-          // Errors.
-          "array-bracket-spacing": [2, "never"],
-          "block-scoped-var": 2,
-          "brace-style": [2, "stroustrup", { allowSingleLine: true }],
-          "comma-dangle": [2, "never"],
-          "comma-spacing": 2,
-          "comma-style": [2, "last"],
-          "computed-property-spacing": [2, "never"],
-          curly: [2, "all"],
-          "eol-last": 2,
-          eqeqeq: [2, "smart"],
-          "guard-for-in": 2,
-          indent: [2, 2, { SwitchCase: 1 }],
-          "key-spacing": [2, { beforeColon: false, afterColon: true }],
-          "linebreak-style": [2, "unix"],
-          "lines-around-comment": [
-            2,
-            { beforeBlockComment: true, afterBlockComment: false },
-          ],
-          "new-parens": 2,
-          "no-array-constructor": 2,
-          "no-caller": 2,
-          "no-catch-shadow": 2,
-          "no-empty-label": 2,
-          "no-eval": 2,
-          "no-extend-native": 2,
-          "no-extra-bind": 2,
-          "no-extra-parens": [2, "functions"],
-          "no-implied-eval": 2,
-          "no-iterator": 2,
-          "no-label-var": 2,
-          "no-labels": 2,
-          "no-lone-blocks": 2,
-          "no-loop-func": 2,
-          "no-multi-spaces": 2,
-          "no-multi-str": 2,
-          "no-native-reassign": 2,
-          "no-nested-ternary": 2,
-          "no-new-func": 2,
-          "no-new-object": 2,
-          "no-new-wrappers": 2,
-          "no-octal-escape": 2,
-          "no-process-exit": 2,
-          "no-proto": 2,
-          "no-return-assign": 2,
-          "no-script-url": 2,
-          "no-sequences": 2,
-          "no-shadow-restricted-names": 2,
-          "no-spaced-func": 2,
-          "no-trailing-spaces": 2,
-          "no-undef-init": 2,
-          "no-undefined": 2,
-          "no-unused-expressions": 2,
-          "no-unused-vars": [2, { vars: "all", args: "none" }],
-          "no-with": 2,
-          "object-curly-spacing": [2, "never"],
-          "one-var": [2, "never"],
-          "quote-props": [2, "consistent-as-needed"],
-          semi: [2, "always"],
-          "semi-spacing": [2, { before: false, after: true }],
-          "space-after-keywords": [2, "always"],
-          "space-before-blocks": [2, "always"],
-          "space-before-function-paren": [
-            2,
-            { anonymous: "always", named: "never" },
-          ],
-          "space-in-parens": [2, "never"],
-          "space-infix-ops": 2,
-          "space-return-throw-case": 2,
-          "space-unary-ops": [2, { words: true, nonwords: false }],
-          "spaced-comment": [2, "always"],
-          strict: 2,
-          yoda: [2, "never"],
-          // Warnings.
-          "max-nested-callbacks": [1, 3],
-          "valid-jsdoc": [
-            1,
-            {
-              prefer: {
-                returns: "return",
-                property: "prop",
-              },
-              requireReturn: false,
-            },
-          ],
-        },
-        fix: true,
-      })
-    )
-    .pipe(eslint.format())
-    // if fixed, write the file to dest
-    .pipe(gulpIf(isFixed, gulp.dest("./js/")));
-});
+function watchFiles() {
+  return watch([
+    "./components/**/*.scss",
+    "./js/**/*.js",
+    "./css/**/*.css",
+    "./components/**/*.js"
+  ],
+    (cb) => {
+      series(minifyImages, lintScripts, lintStyles, buildStyles);
+      cb();
+    });
+}
 
-const sass = require("gulp-sass")(require("sass"));
+exports.imagemin = minifyImages;
+exports.eslint = lintScripts;
+exports.stylelint = lintStyles;
+exports.buildSass = buildStyles;
+exports.sass = series(lintStyles, buildStyles);
+exports.watch = watchFiles;
 
-gulp.task("watch", function () {
-  gulp.watch("./components/**/*.scss", gulp.series("sass"));
-  gulp.watch("./components/**/*.js", gulp.series("eslint"));
-});
+exports.default = parallel(minifyImages, lintScripts, series(lintStyles, buildStyles));
