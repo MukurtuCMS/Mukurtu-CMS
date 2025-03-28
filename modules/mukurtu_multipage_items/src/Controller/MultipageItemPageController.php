524045f4b4a89c9afe00814d4475e50c610b257b
Nate Lampton <nate@lullabot.com>
2025-03-27 23:01:23 -0700

    Fix name of display mode in multipage items.

--- modules/mukurtu_multipage_items/src/Controller/MultipageItemPageController.php
+++ modules/mukurtu_multipage_items/src/Controller/MultipageItemPageController.php
@@ -250,7 +250,7 @@ class MultipageItemPageController extends ControllerBase {
 
     return [
       '#theme' => 'multipage_item_book_view',
-      '#pages' => array_map(fn($p) => $view_builder->view($p, 'browse_grid'), $pages),
+      '#pages' => array_map(fn($p) => $view_builder->view($p, 'grid_browse'), $pages),
       '#page_nav_attributes' => NULL,
       '#table_of_contents' => $toc,
       '#current_page' => $view_builder->view($current_page, 'full'),
