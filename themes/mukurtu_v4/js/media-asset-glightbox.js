1d2b68d38dd43bf0d43420e2b47c27d53387e2c6
Javi-er <javier.reartes@gmail.com>
2025-08-22 17:11:04 -0300

    adjusting rendering

--- themes/mukurtu_v4/js/media-asset-glightbox.js
+++ themes/mukurtu_v4/js/media-asset-glightbox.js
@@ -9,8 +9,39 @@
   "use strict";
 
   function initGLightbox() {
+    // Collect all media elements and build slides
+    const allMediaLinks = document.querySelectorAll('a.media-asset--link');
+    const elements = [];
+    
+    allMediaLinks.forEach(link => {
+      const mediaElement = link.closest('.media--audio, .media--external_embed, .media--document, .media--soundcloud');
+      
+      if (mediaElement) {
+        const content = link.querySelector('.media-asset--content');
+        if (content) {
+          elements.push({
+            content: content.innerHTML,
+            width: 'auto',
+            height: 'auto'
+          });
+        }
+      } else {
+        elements.push({
+          href: link.getAttribute('href')
+        });
+      }
+    });
+    
     const lightbox = new GLightbox({
-      selector: '.media a.button'
+      elements: elements
+    });
+    
+    // Override click handlers to use our lightbox
+    allMediaLinks.forEach((link, index) => {
+      link.addEventListener('click', function(e) {
+        e.preventDefault();
+        lightbox.openAt(index);
+      });
     });
   }
 
