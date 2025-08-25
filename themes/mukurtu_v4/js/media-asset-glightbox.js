dd7392ac9251f13d29113006bcabba83bad9f9bc
Javi-er <javier.reartes@gmail.com>
2025-08-25 09:53:38 -0300

    remove glightbox from every element except video and image

--- themes/mukurtu_v4/js/media-asset-glightbox.js
+++ themes/mukurtu_v4/js/media-asset-glightbox.js
@@ -9,39 +9,8 @@
   "use strict";
 
   function initGLightbox() {
-    // Collect all media elements and build slides
-    const allMediaLinks = document.querySelectorAll('a.media-asset--link');
-    const elements = [];
-
-    allMediaLinks.forEach(link => {
-      const mediaElement = link.closest('.media--audio, .media--external_embed, .media--soundcloud');
-
-      if (mediaElement) {
-        const content = link.querySelector('.media-asset--content');
-        if (content) {
-          elements.push({
-            content: content.innerHTML,
-            width: 'auto',
-            height: 'auto'
-          });
-        }
-      } else {
-        elements.push({
-          href: link.getAttribute('href')
-        });
-      }
-    });
-
     const lightbox = new GLightbox({
-      elements: elements
-    });
-
-    // Override click handlers to use our lightbox
-    allMediaLinks.forEach((link, index) => {
-      link.addEventListener('click', function(e) {
-        e.preventDefault();
-        lightbox.openAt(index);
-      });
+      selector: 'a.media-asset--link'
     });
   }
 
