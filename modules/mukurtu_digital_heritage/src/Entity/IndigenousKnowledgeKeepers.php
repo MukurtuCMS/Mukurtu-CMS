8334af7dadba29f344534e866fb46b7eae82f3a0
Michael Wynne <88502274+michael-wynne-wsu@users.noreply.github.com>
2025-10-02 11:31:12 -0700

    Update IndigenousKnowledgeKeepers.php

--- modules/mukurtu_digital_heritage/src/Entity/IndigenousKnowledgeKeepers.php
+++ modules/mukurtu_digital_heritage/src/Entity/IndigenousKnowledgeKeepers.php
@@ -15,7 +15,7 @@ class IndigenousKnowledgeKeepers extends Paragraph implements IndigenousKnowledg
 
     $definitions['field_name'] = BaseFieldDefinition::create('string')
       ->setLabel(t('Name of the Elder or Knowledge Keeper'))
-      ->setDescription(t(''))
+      ->setDescription(t('The Knowledge Keeper\'s name(s), according to their personal or community preferences. This may include their appropriate title or status. </br>Maximum 255 characters.'))
       ->setSettings([
         'max_length' => 255,
       ])
@@ -29,7 +29,7 @@ class IndigenousKnowledgeKeepers extends Paragraph implements IndigenousKnowledg
 
     $definitions['field_nation'] = BaseFieldDefinition::create('string')
       ->setLabel(t('Nation or Community'))
-      ->setDescription('')
+      ->setDescription('Including the Knowledge Keeper\'s Nation or community foregrounds the importance of relationships when it comes to Indigenous knowledges and recognizes which nations hold which teachings. The  nation/community  element  of  the  citation  not  only  recognizes  the  important  relationship of an Elder or Knowledge Keeper to their nation or community, it helps celebrate the nuances between different community teachings. </br>Maximum 255 characters.')
       ->setSettings([
         'max_length' => 255,
       ])
@@ -42,7 +42,7 @@ class IndigenousKnowledgeKeepers extends Paragraph implements IndigenousKnowledg
 
     $definitions['field_treaty_territory'] = BaseFieldDefinition::create('string')
       ->setLabel(t('Treaty Territory'))
-      ->setDescription('')
+      ->setDescription('For some individuals, or in relation to some teachings, it may be important for the Knowledge Keeper to acknowledge their relationship to a treaty. This may be particularly important regarding oral teachings about land or treaty rights. Only used if applicable and indicated by the Knowledge Keeper. </br>Maximum 255 characters.')
       ->setSettings([
         'max_length' => 255,
       ])
@@ -55,7 +55,7 @@ class IndigenousKnowledgeKeepers extends Paragraph implements IndigenousKnowledg
 
     $definitions['field_living_place'] = BaseFieldDefinition::create('string')
       ->setLabel(t('City or Community They Live in'))
-      ->setDescription(t(''))
+      ->setDescription(t('Knowledge Keepers may live in places that differ from the origins of their knowledge, nation, or birthplace, and, in some cases, this may be an important relationship they want to recognize. Only used if applicable and indicated by the Knowledge Keeper. </br>Maximum 255 characters.'))
       ->setSettings([
         'max_length' => 255,
       ])
@@ -68,7 +68,7 @@ class IndigenousKnowledgeKeepers extends Paragraph implements IndigenousKnowledg
 
     $definitions['field_teaching'] = BaseFieldDefinition::create('string')
       ->setLabel(t('A Brief Description or Title of the Teaching'))
-      ->setDescription('')
+      ->setDescription('Information such as the familial provenance of the teaching could be included here. For example, the citation might read: “Story about the sisters of the river as told to [Name of story keeper] by their grandmother [or the grandmother’s name].” This element should be discussed with the Knowledge Keeper, to properly describe or classify the knowledge they are sharing. </br>Maximum 255 characters.')
       ->setSettings([
         'max_length' => 255,
       ])
@@ -81,7 +81,7 @@ class IndigenousKnowledgeKeepers extends Paragraph implements IndigenousKnowledg
 
     $definitions['field_original_date'] = BaseFieldDefinition::create('original_date')
       ->setLabel(t('Date'))
-      ->setDescription(t(''))
+      ->setDescription(t('As exact a date as possible is preferred to help other scholars working with the same Knowledge Keeper to identify which teaching was being cited. This can also be key if there are temporal or seasonal restrictions to knowledge, which should be discussed in depth with the Knowledge Keeper if they are considering including that knowledge in a publication that may be read at any time. </br>Enter the year and, if known, select the month or month and day.'))
       ->setCardinality(1)
       ->setRequired(FALSE)
       ->setRevisionable(TRUE)
