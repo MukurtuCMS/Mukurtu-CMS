2e91efd6fc4a0718963991ad46195549a690bd19
Matthew Oliveira <m4olivei@gmail.com>
2025-12-04 16:58:34 -0500

    g1224: First pass at style and behavior for the content warnings

--- modules/mukurtu_content_warnings/js/content-warnings.js
+++ modules/mukurtu_content_warnings/js/content-warnings.js
@@ -7,13 +7,15 @@
   Drupal.behaviors.contentWarnings = {
     attach: function (context) {
 
-      once("content-warnings", ".mukurtu-content-warning", context).forEach((e) => {
-        e.addEventListener("click", () => {
-          e.classList.remove("mukurtu-content-warning");
-          // Todo: prob shouldn't rely on the [1] here... need to get this value more reliably
-          e.children[1].classList.add("cleared");
+      once('mukurtu-content-warnings', '.mukurtu-content-warnings', context).forEach((element) => {
+        element.addEventListener('click', (event) => {
+          this.dismissContentWarning(event.target);
         });
       });
+    },
+    dismissContentWarning: function (element) {
+      const mid = element.dataset.mid;
+      document.querySelectorAll(`.mukurtu-content-warnings[data-mid="${mid}"]`).forEach((contentWarnings) => contentWarnings.classList.add('dismissed'));
     },
   };
 })(Drupal, once);
