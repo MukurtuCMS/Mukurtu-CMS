562b923c1c28680cab02c3c0a2b188264efea565
Matthew Oliveira <m4olivei@gmail.com>
2025-12-04 17:16:27 -0500

    g1224: More finessing of the content warning

--- modules/mukurtu_content_warnings/js/content-warnings.js
+++ modules/mukurtu_content_warnings/js/content-warnings.js
@@ -14,7 +14,13 @@
       });
     },
     dismissContentWarning: function (element) {
+      // Exit early if element has a .splide__track--nav parent.
+      if (element.closest('.splide__track--nav')) {
+        return;
+      }
+
       const mid = element.dataset.mid;
+      // Don't dismiss if whats clicked is the splide track nav item.
       document.querySelectorAll(`.mukurtu-content-warnings[data-mid="${mid}"]`).forEach((contentWarnings) => contentWarnings.classList.add('dismissed'));
     },
   };
