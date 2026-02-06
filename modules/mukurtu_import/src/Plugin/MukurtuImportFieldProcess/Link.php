3eee8369c37e166620d0ce0a95c7a15570242b2b
Matthew Oliveira <m4olivei@gmail.com>
2026-02-06 17:16:57 -0500

    1347: Convert MukurtuImportFieldProcess plugins from annotation to attribute based plugins

--- modules/mukurtu_import/src/Plugin/MukurtuImportFieldProcess/Link.php
+++ modules/mukurtu_import/src/Plugin/MukurtuImportFieldProcess/Link.php
@@ -3,21 +3,20 @@
 namespace Drupal\mukurtu_import\Plugin\MukurtuImportFieldProcess;
 
 use Drupal\mukurtu_import\MukurtuImportFieldProcessPluginBase;
+use Drupal\mukurtu_import\Attribute\MukurtuImportFieldProcess;
 use Drupal\Core\Field\FieldDefinitionInterface;
+use Drupal\Core\StringTranslation\TranslatableMarkup;
 
 /**
  * Plugin implementation of the mukurtu_import_field_process.
- *
- * @MukurtuImportFieldProcess(
- *   id = "link",
- *   label = @Translation("Link"),
- *   field_types = {
- *     "link",
- *   },
- *   weight = 0,
- *   description = @Translation("Link.")
- * )
  */
+#[MukurtuImportFieldProcess(
+  id: 'link',
+  label: new TranslatableMarkup('Link'),
+  description: new TranslatableMarkup('Link.'),
+  field_types: ['link'],
+  weight: 0,
+)]
 class Link extends MukurtuImportFieldProcessPluginBase {
   /**
    * {@inheritdoc}
