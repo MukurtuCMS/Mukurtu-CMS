3933d76ef6c93c4bc41a4066b412a90379f69b92
Matthew Oliveira <m4olivei@gmail.com>
2026-02-12 09:50:54 -0500

    1358: Tidy up the mukurtu_imageitem process plugin

--- modules/mukurtu_import/src/Plugin/migrate/process/ImageItem.php
+++ modules/mukurtu_import/src/Plugin/migrate/process/ImageItem.php
@@ -3,32 +3,36 @@
 declare(strict_types = 1);
 
 namespace Drupal\mukurtu_import\Plugin\migrate\process;
+
+use Drupal\file\FileInterface;
+use Drupal\migrate\Attribute\MigrateProcess;
 use Drupal\migrate\MigrateExecutableInterface;
 use Drupal\migrate\ProcessPluginBase;
 use Drupal\migrate\Row;
 
 /**
- * @MigrateProcessPlugin(
- *   id = "mukurtu_imageitem"
- * )
+ * Mukurtu image item processing plugin.
  */
+#[MigrateProcess('mukurtu_imageitem')]
 class ImageItem extends ProcessPluginBase {
+
+  /**
+   * {@inheritdoc}
+   */
   public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
-    $importUriBase = $this->configuration['upload_location'] ?? "private://";
-    $fileStorage = \Drupal::entityTypeManager()->getStorage('file');
+    $import_uri_base = $this->configuration['upload_location'] ?? "private://";
+    $file_storage = \Drupal::entityTypeManager()->getStorage('file');
 
     // Match image info in markdown format: ![alt text](image.jpg "title") OR
     // ![alt text](<image id or uuid> "title")
-    preg_match('/!\[(.*?)\]\s*?\((.+?)\s*([\"].+[\"])?\)/', $value, $matches, PREG_UNMATCHED_AS_NULL);
+    preg_match('/!\[(.*?)]\s*?\((.+?)\s*(?:"(.+?)")?\)/', $value, $matches, PREG_UNMATCHED_AS_NULL);
     $alt = $matches[1] ?? "";
-    // If $title gets a match, it gets double-encapsulated in quotes
-    // (e.g. ""title""), so chop off one set of quotes.
-    $title = $matches[3] ? substr($matches[3], 1, -1) : $alt;
     $image = $matches[2] ?? $value;
+    $title = $matches[3] ?? $alt;
 
     // Check if incoming value is an existing file ID.
     if (is_numeric($image)) {
-      $file = $fileStorage->load($image);
+      $file = $file_storage->load($image);
       if ($file) {
         return [
           'target_id' => $file->id(),
@@ -39,15 +43,15 @@ class ImageItem extends ProcessPluginBase {
     }
 
     // Is this one of the binary files uploaded as part of the import process?
-    $query = $fileStorage->getQuery();
+    $query = $file_storage->getQuery();
     $results = $query->condition('filename', $image)
       // The file must be in the provided upload location scope.
-      ->condition('uri', $importUriBase,'STARTS_WITH')
+      ->condition('uri', $import_uri_base,'STARTS_WITH')
       ->accessCheck(TRUE)
       ->execute();
-    if (!empty($results) && count($results) == 1) {
-      /** @var \Drupal\file\FileInterface $file */
-      if ($file = $fileStorage->load(reset($results))) {
+    if (!empty($results) && count($results) === 1) {
+      $file = $file_storage->load(reset($results));
+      if ($file instanceof FileInterface) {
         return [
           'target_id' => $file->id(),
           'alt' => $alt,
