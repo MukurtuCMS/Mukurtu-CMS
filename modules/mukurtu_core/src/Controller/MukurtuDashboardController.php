7fec35d6349d54b3ef14bf8971f62a1d0fa4e6c3
Nick Deer <nicolette.dixon@wsu.edu>
2026-03-24 16:26:16 -0700

    Restrict dashboard access to authenticated users only

---
+++ modules/mukurtu_core/src/Controller/MukurtuDashboardController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Drupal\mukurtu_core\Controller;
+
+use Drupal\Core\Access\AccessResult;
+use Drupal\Core\Controller\ControllerBase;
+
+class MukurtuDashboardController extends ControllerBase
+{
+
+  /**
+   * Only authenticated users may access the dashboard.
+   *
+   * @return \Drupal\Core\Access\AccessResultInterface
+   *   The access result.
+   */
+  public function access()
+  {
+    $account = \Drupal::currentUser();
+    if (!$account->isAnonymous()) {
+      return AccessResult::allowed();
+    }
+
+    return AccessResult::forbidden();
+  }
+}
