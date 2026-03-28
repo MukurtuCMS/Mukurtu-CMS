dc5cf86cb941168101c31fa0d458b290417a8eb8
Javi-er <javier.reartes@gmail.com>
2026-03-27 22:06:38 -0500

    Adding protocols to dh records and grouping them by protocol

--- modules/mukurtu_local_contexts/src/Plugin/Field/FieldFormatter/LocalContextsLabelFormatter.php
+++ modules/mukurtu_local_contexts/src/Plugin/Field/FieldFormatter/LocalContextsLabelFormatter.php
@@ -6,6 +6,7 @@ use Drupal\Core\Field\FieldItemListInterface;
 use Drupal\Core\Field\FormatterBase;
 use Drupal\mukurtu_local_contexts\LocalContextsLabel;
 use Drupal\mukurtu_local_contexts\LocalContextsNotice;
+use Drupal\mukurtu_local_contexts\LocalContextsProject;
 
 /**
  * Plugin implementation of the 'Local Contexts Label and Notice' formatter.
@@ -23,18 +24,14 @@ class LocalContextsLabelFormatter extends FormatterBase {
    * {@inheritdoc}
    */
   public function viewElements(FieldItemListInterface $items, $langcode) {
-    $element = [];
-    $first = '';
-    $second = '';
+    $grouped = [];
 
-    foreach ($items as $delta => $item) {
-      list($first, $second, $third) = explode(':', $item->value);
-      // $item->value = project_id:label_id_or_notice_type:label_or_notice.
+    foreach ($items as $item) {
+      [$project_id, , $third] = explode(':', $item->value);
 
-      // Check if third value in the $item->value throuple is a label or notice.
-      if ($third == 'label') {
+      if ($third === 'label') {
         $label = new LocalContextsLabel($item->value);
-        $element[$delta] = [
+        $grouped[$project_id][] = [
           '#theme' => 'local_contexts_label',
           '#name' => $label->name,
           '#text' => $label->default_text,
@@ -43,16 +40,29 @@ class LocalContextsLabelFormatter extends FormatterBase {
           '#translations' => $label->translations,
         ];
       }
-      else if ($third == 'notice') {
+      elseif ($third === 'notice') {
         $notice = new LocalContextsNotice($item->value);
-        $element[$delta] = [
+        $grouped[$project_id][] = [
           '#theme' => 'local_contexts_notice',
           '#name' => $notice->name,
           '#text' => $notice->default_text,
           '#svg_url' => $notice->svg_url,
+          '#img_url' => $notice->img_url,
           '#translations' => $notice->translations,
         ];
       }
+    }
+
+    $element = [];
+    $delta = 0;
+    foreach ($grouped as $project_id => $group_items) {
+      $project = new LocalContextsProject($project_id);
+      $element[$delta] = [
+        '#theme' => 'local_contexts_label_group',
+        '#project_title' => $project->isValid() ? $project->getTitle() : null,
+        '#items' => $group_items,
+      ];
+      $delta++;
     }
 
     return $element;
