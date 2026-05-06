ec9afc59c87db8a9c2032b8c0edaf980d189e25d
Michael Wynne <88502274+michael-wynne-wsu@users.noreply.github.com>
2026-05-06 13:50:17 -0700

    Adding a new trigger to send users email if their passwords get reset

---
+++ modules/mukurtu_protocol/src/Plugin/Action/MukurtuUserSetRandomPasswordAction.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Drupal\mukurtu_protocol\Plugin\Action;
+
+use Drupal\Core\Action\ActionBase;
+use Drupal\Core\Password\PasswordGeneratorInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\Session\AccountInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Replaces genpass's UserSetRandomPassword to also send a one-time login email.
+ */
+class MukurtuUserSetRandomPasswordAction extends ActionBase implements ContainerFactoryPluginInterface {
+
+  public function __construct(
+    array $configuration,
+    $plugin_id,
+    $plugin_definition,
+    protected PasswordGeneratorInterface $passwordGenerator,
+  ) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(
+    ContainerInterface $container,
+    array $configuration,
+    $plugin_id,
+    $plugin_definition,
+  ) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('password_generator'),
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function execute($account = NULL) {
+    if ($account !== FALSE && $account !== NULL) {
+      $account->original = clone $account;
+      $account->setPassword($this->passwordGenerator->generate());
+      $account->save();
+
+      _user_mail_notify('password_reset', $account);
+
+      $this->messenger()->addStatus($this->t(
+        'A password reset email has been sent to %name.', [
+          '%name' => $account->getDisplayName(),
+        ]
+      ));
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function access($object, ?AccountInterface $account = NULL, $return_as_object = FALSE) {
+    $access = $object->status->access('edit', $account, TRUE)
+      ->andIf($object->access('update', $account, TRUE));
+
+    return $return_as_object ? $access : $access->isAllowed();
+  }
+
+}
